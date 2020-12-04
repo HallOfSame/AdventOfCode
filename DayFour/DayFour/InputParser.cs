4f5b474fd01704ec694d64311c34e5daada050c8
Dylan <samuel.ogara94@gmail.com>
2020-12-04 08:53:15 -0500

    Day 4 part one done

---
+++ DayFour/DayFour/InputParser.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DayFour
+{
+    public class InputParser
+    {
+        #region Instance Methods
+
+        public List<PassportModel> ParseFile(StreamReader fileStream)
+        {
+            var passportLines = ParseLinesFromFile(fileStream: fileStream);
+
+            var passports = GetPassportModelsFromLineData(passportLines: passportLines);
+
+            return passports;
+        }
+
+        private List<PassportModel> GetPassportModelsFromLineData(List<List<string>> passportLines)
+        {
+            var passports = new List<PassportModel>();
+
+            foreach (var passport in passportLines)
+            {
+                var passportModel = new PassportModel();
+
+                foreach (var line in passport)
+                {
+                    var pairs = line.Split(' ');
+
+                    foreach (var pair in pairs)
+                    {
+                        var splitPair = pair.Split(':');
+
+                        passportModel.AddInfo(splitPair[0],
+                                              splitPair[1]);
+                    }
+                }
+
+                passports.Add(passportModel);
+            }
+
+            return passports;
+        }
+
+        #endregion
+
+        #region Class Methods
+
+        private static List<List<string>> ParseLinesFromFile(StreamReader fileStream)
+        {
+            var passportLines = new List<List<string>>();
+
+            var currentPassport = new List<string>();
+
+            while (!fileStream.EndOfStream)
+            {
+                var line = fileStream.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    passportLines.Add(currentPassport);
+                    currentPassport = new List<string>();
+                    continue;
+                }
+
+                currentPassport.Add(line);
+            }
+
+            // Since we end the while loop before adding this last one
+            passportLines.Add(currentPassport);
+
+            return passportLines;
+        }
+
+        #endregion
+    }
+}
---
+++ DayFour/DayFour/PassportValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace DayFour
+{
+    public class PassportValidator
+    {
+        #region Fields
+
+        private readonly HashSet<string> requiredKeys;
+
+        #endregion
+
+        #region Constructors
+
+        public PassportValidator(List<string> requiredKeys)
+        {
+            this.requiredKeys = new HashSet<string>(requiredKeys);
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        public bool IsPassportValid(PassportModel passportModel)
+        {
+            var passportKeys = new HashSet<string>(passportModel.KeyValues.Keys);
+
+            // If the keys of the passport is a super set, then it has all the required keys
+            var valid = passportKeys.IsSupersetOf(requiredKeys);
+
+            return valid;
+        }
+
+        #endregion
+    }
+}
