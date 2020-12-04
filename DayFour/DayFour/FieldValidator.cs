554d1a9eae3a510fbf215bb036ebc9db595f1737
Dylan <samuel.ogara94@gmail.com>
2020-12-04 08:53:15 -0500

    Day 4 part one done

---
+++ DayFour/DayFour/FieldValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayFour
+{
+    public class FieldValidator
+    {
+        private readonly Func<bool, string> fieldValidationMethod;
+
+        private readonly string key;
+
+        public FieldValidator(string key, Func<bool, string> fieldValidation)
+        {
+            this.key = key;
+            this.fieldValidationMethod = fieldValidation;
+        }
+    }
+}
---
+++ DayFour/DayFour/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DayFour
+{
+    internal class Program
+    {
+        #region Class Methods
+
+        private static void Main(string[] args)
+        {
+            List<PassportModel> passportModels;
+
+            using (var file = File.OpenRead("PuzzleInput.txt"))
+            using (var reader = new StreamReader(file))
+            {
+                var parser = new InputParser();
+
+                passportModels = parser.ParseFile(reader);
+            }
+
+            var partOneValidator = new PassportValidator(new List<string>
+                                                         {
+                                                             "byr",
+                                                             "iyr",
+                                                             "eyr",
+                                                             "hgt",
+                                                             "hcl",
+                                                             "ecl",
+                                                             "pid"
+                                                         });
+
+            var partOneValidPassports = passportModels.Count(partOneValidator.IsPassportValid);
+
+            Console.WriteLine($"Part one valid passports: {partOneValidPassports}.");
+
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
