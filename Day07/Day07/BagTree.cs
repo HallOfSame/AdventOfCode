6f03cc217923af7ddec7833ed0bc100833c72bcf
HallOfSame <samuel.ogara94@gmail.com>
2020-12-07 17:10:15 -0500

    Making the code a little more respectable

--- Day07/Day07/BagTree.cs
+++ Day07/Day07/BagTree.cs
@@ -1,10 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Day07
 {
-    class BagTree
+    // 'Tree' might not be entirely accurate
+    public class BagTree
     {
+        #region Fields
+
+        private readonly Dictionary<string, BagNode> allNodes;
+
+        private readonly Dictionary<string, int> bagRequirementCache;
+
+        private readonly Dictionary<string, List<string>> parentColorCache;
+
+        #endregion
+
+        #region Constructors
+
+        public BagTree(IEnumerable<BagNode> nodes)
+        {
+            allNodes = nodes.ToDictionary(x => x.Color,
+                                          x => x);
+
+            parentColorCache = new Dictionary<string, List<string>>();
+            bagRequirementCache = new Dictionary<string, int>();
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        public List<string> GetAllowedParentColorsForColor(string color)
+        {
+            if (parentColorCache.TryGetValue(color,
+                                             out var allowedParents))
+            {
+                return allowedParents;
+            }
+
+            var allowedOuterBagColors = new HashSet<string>();
+
+            var startNode = allNodes[color];
+
+            foreach (var parent in startNode.ParentLinks)
+            {
+                // This node color is always allowed
+                var allowedColors = new List<string>
+                                    {
+                                        parent.LinkedNode.Color
+                                    };
+
+                allowedColors.AddRange(GetAllowedParentColorsForColor(parent.LinkedNode.Color));
+
+                allowedOuterBagColors.UnionWith(allowedColors);
+            }
+
+            var outerColorList = allowedOuterBagColors.ToList();
+
+            parentColorCache[color] = outerColorList;
+
+            return outerColorList;
+        }
+
+        public int GetNumberOfBagsRequiredForColor(string color)
+        {
+            var startNode = allNodes[color];
+
+            if (bagRequirementCache.TryGetValue(color,
+                                                out var requiredBags))
+            {
+                return requiredBags;
+            }
+
+            if (!startNode.ChildNodes.Any())
+            {
+                // No bags required inside this color
+                return 0;
+            }
+
+            var numberOfBags = 0;
+
+            foreach (var child in startNode.ChildNodes)
+            {
+                // First get the number of bags required inside this color bag
+                var numberOfBagsForColor = GetNumberOfBagsRequiredForColor(child.LinkedNode.Color);
+
+                // Multiply that by the number of this color we need. Then add the quantity of the child color we have
+                var numberOfColoredBagsForThisNode = numberOfBagsForColor * child.Quantity + child.Quantity;
+
+                numberOfBags += numberOfColoredBagsForThisNode;
+            }
+
+            bagRequirementCache[color] = numberOfBags;
+
+            return numberOfBags;
+        }
+
+        #endregion
     }
 }
---
+++ Day07/Day07/BagTreeFactory.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day07
+{
+    public class BagTreeFactory
+    {
+        #region Instance Methods
+
+        public BagTree BuildTree(IList<BagRuleModel> ruleModels)
+        {
+            // Build tree with nodes
+            var modelDictionary = ruleModels.ToDictionary(x => x.BagColor,
+                                                          x => x);
+
+            // Top level will be bag colors where no other bag can contain them
+            var topLevelBags = ruleModels.Where(top => !ruleModels.Any(mod => mod.AllowedContents.Any(content => content.color == top.BagColor)));
+
+            var allNodes = new List<BagNode>();
+
+            foreach (var topLevel in topLevelBags)
+            {
+                allNodes.Add(BuildNodeFromModel(topLevel,
+                                                allNodes,
+                                                modelDictionary));
+            }
+
+            return new BagTree(allNodes);
+        }
+
+        private BagNode BuildNodeFromModel(BagRuleModel bagRuleModel,
+                                           IList<BagNode> allNodes,
+                                           IDictionary<string, BagRuleModel> modelDictionary)
+        {
+            var node = new BagNode(bagRuleModel.BagColor);
+
+            foreach (var (color, qty) in bagRuleModel.AllowedContents)
+            {
+                var childNode = GetNodeForColor(color,
+                                                allNodes,
+                                                modelDictionary);
+
+                node.ChildNodes.Add(new NodeLink(childNode,
+                                                 qty));
+
+                childNode.ParentLinks.Add(new NodeLink(node,
+                                                       1));
+            }
+
+            return node;
+        }
+
+        private BagNode GetNodeForColor(string color,
+                                        IList<BagNode> allNodes,
+                                        IDictionary<string, BagRuleModel> modelDictionary)
+        {
+            var existingNode = allNodes.FirstOrDefault(x => x.Color == color);
+
+            if (existingNode != null)
+            {
+                return existingNode;
+            }
+
+            var newNode = BuildNodeFromModel(modelDictionary[color],
+                                             allNodes,
+                                             modelDictionary);
+            allNodes.Add(newNode);
+            return newNode;
+        }
+
+        #endregion
+    }
+}
