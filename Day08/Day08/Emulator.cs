deb158da9d4ab0441b62fa6d201713cc72311b99
HallOfSame <samuel.ogara94@gmail.com>
2020-12-08 08:34:01 -0500

    Day 8 pt 1

---
+++ Day08/Day08/Emulator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day08
+{
+    public class Emulator
+    {
+        #region Fields
+
+        private readonly HashSet<int> lineNumbersExecuted;
+
+        #endregion
+
+        #region Constructors
+
+        public Emulator()
+        {
+            lineNumbersExecuted = new HashSet<int>();
+        }
+
+        #endregion
+
+        #region Enums
+
+        public enum ResultCode
+        {
+            ExecutionComplete,
+
+            Deadlock,
+
+            Fault
+        }
+
+        #endregion
+
+        #region Instance Properties
+
+        public int AccumulatorValue { get; private set; }
+
+        #endregion
+
+        #region Instance Methods
+
+        public ResultCode RunOperations(List<Operation> operations)
+        {
+            AccumulatorValue = 0;
+            lineNumbersExecuted.Clear();
+
+            var lineNumberToExecute = 0;
+
+            ResultCode result;
+
+            while (true)
+            {
+                if (lineNumberToExecute < 0)
+                {
+                    // Shouldn't happen
+                    result = ResultCode.Fault;
+                    break;
+                }
+
+                if (lineNumberToExecute == operations.Count + 1)
+                {
+                    // We've reached the end of the file
+                    result = ResultCode.ExecutionComplete;
+                    break;
+                }
+
+                if (lineNumbersExecuted.Contains(lineNumberToExecute))
+                {
+                    // We've hit a deadlock
+                    result = ResultCode.Deadlock;
+                    break;
+                }
+
+                lineNumbersExecuted.Add(lineNumberToExecute);
+
+                var operationToExecute = operations[lineNumberToExecute];
+
+                switch (operationToExecute.OperationType)
+                {
+                    case OperationType.NOP:
+                        lineNumberToExecute++;
+                        break;
+                    case OperationType.JMP:
+                        lineNumberToExecute += operationToExecute.Argument;
+                        break;
+                    case OperationType.ACC:
+                        AccumulatorValue += operationToExecute.Argument;
+                        lineNumberToExecute++;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"Invalid operation type {operationToExecute.OperationType} detected.");
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Day08/Day08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day08
+{
+    internal class Program
+    {
+        #region Class Methods
+
+        public static async Task Main(string[] args)
+        {
+            List<Operation> operations;
+
+            await using (var file = File.OpenRead("PuzzleInput.txt"))
+            {
+                using var reader = new StreamReader(file);
+
+                operations = await new InputParser().ReadInput(reader);
+            }
+
+            var emulator = new Emulator();
+
+            // Pt 1
+            var result = emulator.RunOperations(operations);
+
+            Console.WriteLine($"Result of running operations {result}. Accumulator at {emulator.AccumulatorValue}.");
+
+            // Pt 2
+
+
+        }
+
+        #endregion
+    }
+}
