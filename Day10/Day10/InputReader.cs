69fac57773cbe56503392b4f6b4d6b002f6f0e3c
Dylan <samuel.ogara94@gmail.com>
2020-12-10 07:38:15 -0500

    Day 10 pt 1

---
+++ Day10/Day10/InputReader.cs
@@ -0,0 +1,16 @@
+﻿using Helpers;
+
+namespace Day10
+{
+    public class InputReader : FileReader<int>
+    {
+        #region Instance Methods
+
+        protected override int ProcessLineOfFile(string line)
+        {
+            return int.Parse(line);
+        }
+
+        #endregion
+    }
+}
---
+++ Day10/Day10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var input = await new InputReader().ReadInputFromFile();
+
+
+            // PT 1
+            input.Sort();
+
+            var currentJolt = 0;
+            var diff1Count = 0;
+            var diff3Count = 0;
+
+            foreach (var jolt in input)
+            {
+                var joltDifference = jolt - currentJolt;
+
+                var valid = joltDifference <= 3;
+
+                if (!valid)
+                {
+                    throw new InvalidOperationException($"Can't use all because a difference of {joltDifference} was found.");
+                }
+
+                currentJolt = jolt;
+
+                if (joltDifference == 1)
+                {
+                    diff1Count++;
+                }
+                else if (joltDifference == 3)
+                {
+                    diff3Count++;
+                }
+            }
+
+            // Last adapter to input always adds an extra diff of 3
+            diff3Count++;
+
+            Console.WriteLine($"Diff 1 {diff1Count} with Diff 3 {diff3Count}. Puzzle result {diff1Count * diff3Count}.");
+        }
+    }
+}
