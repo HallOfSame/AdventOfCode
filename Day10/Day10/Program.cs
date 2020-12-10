1aee5237b91e2e096f0f20930207c8884a60a6c9
Dylan <samuel.ogara94@gmail.com>
2020-12-10 09:26:37 -0500

    Reversing the processing makes it real fast. Day 10 Pt 2.

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -49,67 +49,36 @@ namespace Day10
 
             // PT 2
 
-            var forcedConnections = input.Where((x,
-                                                 idx) =>
-                                                {
-                                                    if (idx - 1 < 0)
-                                                    {
-                                                        // First item in the list only cares about if we have to have it to connect to the second
-                                                        return input[idx + 1] - x == 3;
-                                                    }
-                                                    else if (idx + 1 >= input.Count)
-                                                    {
-                                                        // Final one always needs to connect
-                                                        return true;
-                                                    }
+            var deviceJolt = input.Max() + 3;
 
-                                                    // If the next item is 3 away from the second we always have to hook up the first adapter
-                                                    var requiredToConnectToNext = input[idx + 1] - x == 3;
+            input.Reverse();
 
-                                                    // Similarly if the prev item is 3 away we must always connect our current adapter
-                                                    var requiredToConnectToPrev = input[idx - 1] - x == -3;
+            var joltPaths = new Dictionary<int, long>
+                            {
+                                {
+                                    deviceJolt, 1
+                                }
+                            };
 
-                                                    return requiredToConnectToNext || requiredToConnectToPrev;
-                                                })
-                                         .ToList();
-
-            var optionalAdapters = input.Where(x => !forcedConnections.Contains(x))
-                                        .ToList();
-
-            // Math time
-            // We want to figure out from our list of optional adapters "how many ways could we have just N of them plugged in" out of X optional adapters
-            // Thanks to S/O this is a binomial coefficient
-            // Formula: C = (X!) / (N! * (X-N)!)
-            var combinations = 0;
-
-            decimal GetFactorial(int num)
+            foreach (var jolt in input)
             {
-                if (num == 0)
-                {
-                    return 1;
-                }
+                joltPaths.TryGetValue(jolt + 1,
+                                      out var pathsToOneJolt);
 
-                return Enumerable.Range(1,
-                                        num)
-                                 .Aggregate(1m,
-                                            (fac,
-                                             val) => fac * val);
+                joltPaths.TryGetValue(jolt + 2,
+                                      out var pathsToTwoJolts);
+
+                joltPaths.TryGetValue(jolt + 3,
+                                      out var pathsToThreeJolts);
+
+                joltPaths[jolt] = pathsToOneJolt + pathsToTwoJolts + pathsToThreeJolts;
             }
 
-            var xFactorial = GetFactorial(optionalAdapters.Count);
+            var validStarts = input.Where(x => x <= 3)
+                                   .ToList();
 
-            for (var i = 0; i <= optionalAdapters.Count; i++)
-            {
-                var nFactorial = GetFactorial(i);
-
-                var xMinusNFactorial = GetFactorial(optionalAdapters.Count - i);
-
-                var combinationsAtI = (xFactorial) / (nFactorial * xMinusNFactorial);
-
-                Console.WriteLine($"Combinations at {i}: {combinationsAtI}");
-
-                combinations += (int)combinationsAtI;
-            }
+            var combinations = validStarts.Select(x => joltPaths[x])
+                                          .Sum();
             
             Console.WriteLine($"Combinations of hookups for our device {combinations}.");
         }
