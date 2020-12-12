db9b756fc4acef0bdefdddf3f967ce19f1636d54
HallOfSame <samuel.ogara94@gmail.com>
2020-12-12 08:38:37 -0500

    Day 12 pt 1

---
+++ Day12/Day12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day12
+{
+    internal class Program
+    {
+        #region Class Methods
+
+        private static void Main(string[] args)
+        {
+            var fileText = File.ReadAllText("PuzzleInput.txt");
+
+            MoveDirection GetMoveFromChar(char move)
+            {
+                return move switch
+                {
+                    'N' => MoveDirection.North,
+                    'S' => MoveDirection.South,
+                    'W' => MoveDirection.West,
+                    'E' => MoveDirection.East,
+                    'R' => MoveDirection.Right,
+                    'L' => MoveDirection.Left,
+                    'F' => MoveDirection.Forward,
+                    _ => throw new ArgumentOutOfRangeException(nameof(move))
+                };
+            }
+
+            var moves = fileText.Split(Environment.NewLine)
+                                .Select(x => new ShipMove(GetMoveFromChar(x[0]),
+                                                          int.Parse(x[1..])))
+                                .ToList();
+
+            var ship = new Ship();
+
+            foreach (var move in moves)
+            {
+                ship.MoveShip(move);
+            }
+
+            var location = ship.GetCurrentPosition();
+
+            var distance = Math.Abs(location.x) + Math.Abs(location.y);
+
+            Console.WriteLine($"Distance from start: {distance}.");
+        }
+
+        #endregion
+    }
+}
---
+++ Day12/Day12/Ship.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Day12
+{
+    public class Ship
+    {
+        #region Fields
+
+        private MoveDirection currentFacing;
+
+        private int xPos;
+
+        private int yPos;
+
+        #endregion
+
+        #region Constructors
+
+        public Ship()
+        {
+            xPos = 0;
+            yPos = 0;
+            currentFacing = MoveDirection.East;
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        public (int x, int y) GetCurrentPosition()
+        {
+            return (xPos, yPos);
+        }
+
+        public void MoveShip(ShipMove move)
+        {
+            if (move.Direction == MoveDirection.Left
+                || move.Direction == MoveDirection.Right)
+            {
+                var turnsToMake = move.Amount / 90;
+
+                int turnModifier;
+
+                if (move.Direction == MoveDirection.Left)
+                {
+                    // 3 rights make a left
+                    turnModifier = 3;
+                }
+                else
+                {
+                    turnModifier = 1;
+                }
+
+                var currentFacingInt = (int)currentFacing;
+
+                currentFacingInt += turnsToMake * turnModifier;
+
+                currentFacingInt = Math.Abs(currentFacingInt);
+
+                Math.DivRem(currentFacingInt,
+                            4,
+                            out currentFacingInt);
+
+                currentFacing = (MoveDirection)currentFacingInt;
+                return;
+            }
+
+            var directionToMove = move.Direction;
+
+            if (directionToMove == MoveDirection.Forward)
+            {
+                directionToMove = currentFacing;
+            }
+
+            switch (directionToMove)
+            {
+                case MoveDirection.North:
+                    xPos += move.Amount;
+                    break;
+                case MoveDirection.South:
+                    xPos -= move.Amount;
+                    break;
+                case MoveDirection.East:
+                    yPos += move.Amount;
+                    break;
+                case MoveDirection.West:
+                    yPos -= move.Amount;
+                    break;
+                default:
+                    throw new InvalidOperationException($"Invalid move direction {directionToMove}.");
+            }
+        }
+
+        #endregion
+    }
+
+    public class ShipMove
+    {
+        #region Constructors
+
+        public ShipMove(MoveDirection direction,
+                        int amount)
+        {
+            Direction = direction;
+            Amount = amount;
+        }
+
+        #endregion
+
+        #region Instance Properties
+
+        public int Amount { get; }
+
+        public MoveDirection Direction { get; }
+
+        #endregion
+    }
+
+    public enum MoveDirection
+    {
+        Left = -1,
+
+        Right = -2,
+
+        Forward = -3,
+
+        North = 0,
+
+        South = 2,
+
+        East = 1,
+
+        West = 3
+    }
+}
