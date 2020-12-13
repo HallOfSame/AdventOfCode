cccfcc6cb74ffdc205ecb38e3000a74c13f69efb
Dylan <samuel.ogara94@gmail.com>
2020-12-13 07:51:01 -0500

    Day 13 pt 1

---
+++ Day13/Day13/Bus.cs
@@ -0,0 +1,24 @@
+﻿namespace Day13
+{
+    public class Bus
+    {
+        #region Constructors
+
+        public Bus(int nextDeparture,
+                   int id)
+        {
+            NextDeparture = nextDeparture;
+            Id = id;
+        }
+
+        #endregion
+
+        #region Instance Properties
+
+        public int Id { get; }
+
+        public int NextDeparture { get; }
+
+        #endregion
+    }
+}
---
+++ Day13/Day13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileText = File.ReadAllText("PuzzleInput.txt");
+
+            var fileSplit = fileText.Split(Environment.NewLine);
+
+            var timeYouCanDepart = int.Parse(fileSplit[0]);
+
+            var availableBuses = fileSplit[1]
+                                 .Split(',')
+                                 .Where(x => x != "x")
+                                 .Select(int.Parse)
+                                 .ToList();
+
+            var busList = new List<Bus>();
+
+            foreach (var busId in availableBuses)
+            {
+                var tempTime = timeYouCanDepart;
+
+
+                Math.DivRem(tempTime,
+                            busId,
+                            out var remainder);
+
+                tempTime = tempTime + (busId - remainder);
+
+                busList.Add(new Bus(tempTime,
+                                    busId));
+            }
+
+            // PT 1
+
+            var busYouCanTake = busList.OrderBy(x => x.NextDeparture - timeYouCanDepart)
+                                       .First();
+
+            var waitTime = busYouCanTake.NextDeparture - timeYouCanDepart;
+
+            Console.WriteLine($"You can take bus ID {busYouCanTake.Id} after waiting {waitTime} min. Puzzle result: {busYouCanTake.Id * waitTime}.");
+        }
+    }
+}
