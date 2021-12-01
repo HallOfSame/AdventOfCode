ecb5fac869be5433b3b6ceb73ff4ac83629c970d
Dylan O'Gara <dogara@bastiansolutions.com>
2021-12-01 09:42:49 -0500

    2021 Day 1 Part 1

--- 2021/Day01/Program.cs
+++ 2021/Day01/Program.cs
@@ -13,4 +13,23 @@ for (var i = 1; i < fileLines.Count; i++)
     }
 }
 
-Console.WriteLine(increases);
+Console.WriteLine($"Part 1: {increases}.");
+
+// Part 2
+increases = 0;
+
+var previousWindow = fileLines[0] + fileLines[1] + fileLines[2];
+
+for (var i = 3; i < fileLines.Count; i++)
+{
+    var currentWindow = fileLines[i - 2] + fileLines[i - 1] + fileLines[i];
+
+    if (previousWindow < currentWindow)
+    {
+        increases++;
+    }
+
+    previousWindow = currentWindow;
+}
+
+Console.WriteLine($"Part 2: {increases}.");
