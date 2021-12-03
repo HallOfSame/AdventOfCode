70e469f08c4a28152a1a301392747d9be0e86210
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-02 21:44:37 -0500

    Day 07 Part 2 Redone

--- PuzzleDays/Day07/Program.cs
+++ PuzzleDays/Day07/Program.cs
@@ -49,7 +49,8 @@ foreach (var phaseSetting in possiblePhaseSettings)
                 case 1:
                     return currentSignal;
                 default:
-                    throw new InvalidOperationException("Called for input more than twice.");
+                    Console.Write("Called for input more than twice in part 1."); // Don't throw so we can run the examples
+                    return 0;
             }
         }, (output) =>
         {
@@ -60,6 +61,90 @@ foreach (var phaseSetting in possiblePhaseSettings)
     if (currentSignal > maxOutput)
     {
         maxOutput = currentSignal;
+    }
+}
+
+Console.WriteLine($"Max output: {maxOutput}");
+
+// Part 2
+
+maxOutput = 0;
+
+var feedbackPhases = new List<int> { 5, 6, 7, 8, 9 };
+
+var possibleFeedbackSettings = Permutate(feedbackPhases).ToList();
+
+foreach (var phaseSetting in possibleFeedbackSettings)
+{
+    var thrusters = new (Interpreter, ManualResetEvent)[phaseSetting.Count];
+
+    for(var i = 0; i < phaseSetting.Count; i++)
+    {
+        var thruster = new Interpreter();
+        thrusters[i] = (thruster, new ManualResetEvent(false));
+    }
+
+    var output = new int[phaseSetting.Count];
+
+    var tasks = new List<Task>();
+
+    for (var i = 0; i < phaseSetting.Count; i++)
+    {
+        // J is the index of the thruster we are receiving from
+        var j = i - 1;
+
+        // Closures are mean
+        var x = i;
+
+        if (j < 0)
+        {
+            j = phaseSetting.Count - 1;
+        }
+
+        var firstCall = true;
+        var secondCall = false;
+
+        var thrusterMethod = () =>
+        {
+            thrusters[x].Item1.ProcessOperations(program, () =>
+            {
+                if (firstCall)
+                {
+                    firstCall = false;
+                    secondCall = true;
+                    return phaseSetting[x];
+                }
+
+                if (secondCall && x == 0)
+                {
+                    // Send 0 to first thruster once
+                    secondCall = false;
+                    return 0;
+                }
+
+                thrusters[j].Item2.WaitOne();
+                thrusters[j].Item2.Reset();
+
+                return output[j];
+            }, (val) =>
+            {
+                output[x] = val;
+                thrusters[x].Item2.Set();
+            });
+        };
+
+        var thrusterTask = Task.Run(thrusterMethod);
+
+        tasks.Add(thrusterTask);
+    }
+
+    Task.WaitAll(tasks.ToArray());
+
+    var endOutput = output[phaseSetting.Count - 1];
+
+    if (endOutput > maxOutput)
+    {
+        maxOutput = endOutput;
     }
 }
 
