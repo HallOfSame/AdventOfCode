ba288f10d7e51a5ed13a1f697075a700a697a473
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-04 08:28:58 -0500

    2021 Day 04 Part 2

--- 2021/Day04/Program.cs
+++ 2021/Day04/Program.cs
@@ -9,7 +9,7 @@ var currentDrawnNumbers = new HashSet<int>();
 // Add first four, can't possibly have a winner yet
 sequence.Take(4).ToList().ForEach(x => currentDrawnNumbers.Add(x));
 
-// Part 1
+var foundFirstWinner = false;
 
 for(var i = 4; i < sequence.Count; i++)
 {
@@ -17,14 +17,38 @@ for(var i = 4; i < sequence.Count; i++)
 
     currentDrawnNumbers.Add(nextNumber);
 
-    var winningBoard = boards.FirstOrDefault(x => x.IsWinner(currentDrawnNumbers));
-
-    if (winningBoard != null)
+    if (!foundFirstWinner)
     {
-        Console.WriteLine($"Part 1 Winner: {winningBoard.GetFinalScore(nextNumber, currentDrawnNumbers)}.");
+        // Part 1
+        var winningBoard = boards.FirstOrDefault(x => x.IsWinner(currentDrawnNumbers));
 
-        Console.WriteLine(winningBoard.ToString());
+        if (winningBoard != null)
+        {
+            Console.WriteLine($"Part 1 Winner: {winningBoard.GetFinalScore(nextNumber, currentDrawnNumbers)}.");
 
-        break;
+            Console.WriteLine(winningBoard.ToString());
+
+            foundFirstWinner = true;
+        }
+    }
+    else
+    {
+        // Keep processing for part 2        
+
+        if (boards.Count == 1 && boards[0].IsWinner(currentDrawnNumbers))
+        {
+            var losingBoard = boards[0];
+
+            Console.WriteLine($"Part 2 Losing Board: {losingBoard.GetFinalScore(nextNumber, currentDrawnNumbers)}.");
+
+            Console.WriteLine(losingBoard.ToString());
+
+            break;
+        }
+        else
+        {
+            // Don't waste time processing boards that already won
+            boards.RemoveAll(x => x.IsWinner(currentDrawnNumbers));
+        }
     }
 }
