0bf5a77f8533d12e1be3b1dad695ff42cb6033dc
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-05 00:52:36 -0500

    2021 Day 05 Part 1

---
+++ 2021/Day05/Line.cs
@@ -0,0 +1,100 @@
+﻿using Helpers;
+using System.Text;
+
+namespace Day05
+{
+    internal class Field
+    {
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public int[,] FloorData { get; }
+
+        public Field(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            FloorData = new int[Height, Width];
+        }
+
+        public int[] GetRow(int rowNumber)
+        {
+            return Enumerable.Range(0, Width)
+                    .Select(x => FloorData[x, rowNumber])
+                    .ToArray();
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            void AppendBorder()
+            {
+                stringBuilder.AppendLine(new string(Enumerable.Repeat('-', Width).ToArray()));
+            }
+
+            AppendBorder();
+
+            for(var i = 0; i < Height; i++)
+            {
+                var row = GetRow(i);
+                stringBuilder.AppendLine(string.Join(string.Empty, row));
+            }
+
+            AppendBorder();
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    internal class Coordinate
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+
+    internal class Line
+    {
+        public Coordinate Start { get; set; }
+
+        public Coordinate End { get; set; }
+    }
+
+    internal class LineReader : FileReader<Line>
+    {
+        protected override Line ProcessLineOfFile(string line)
+        {
+            var lineSplits = line.Split(" -> ").Select(s => s.Split(',')).ToArray();
+
+            var start = new Coordinate
+            {
+                X = int.Parse(lineSplits[0][0]),
+                Y = int.Parse(lineSplits[0][1])
+            };
+
+            var end = new Coordinate
+            {
+                X = int.Parse(lineSplits[1][0]),
+                Y = int.Parse(lineSplits[1][1])
+            };
+
+            if (start.X > end.X || start.Y > end.Y)
+            {
+                // Coordinates in the data are not in order
+                return new Line
+                {
+                    Start = end,
+                    End = start
+                };
+            }
+
+            return new Line
+            {
+                Start = start,
+                End = end
+            };
+        }
+    }
+}
---
+++ 2021/Day05/Program.cs
@@ -0,0 +1,44 @@
+﻿using Day05;
+
+var lines = await new LineReader().ReadInputFromFile();
+
+var width = lines.SelectMany(x => new int[] { x.Start.X, x.End.X }).Max() + 1;
+
+var heigth = lines.SelectMany(x => new int[] { x.Start.Y, x.End.Y }).Max() + 1;
+
+var field = new Field(width, heigth);
+
+// Part 1
+
+foreach(var line in lines)
+{
+    if (line.Start.X == line.End.X)
+    {
+        // Vertical
+        for(var i = line.Start.Y; i <= line.End.Y; i++)
+        {
+            field.FloorData[line.Start.X, i] += 1;
+        }
+    }
+    else if (line.Start.Y == line.End.Y)
+    {
+        // Horizontal
+        for (var i = line.Start.X; i <= line.End.X; i++)
+        {
+            field.FloorData[i, line.Start.Y] += 1;
+        }
+    }
+    else
+    {
+        // Part 1, ignore diagonals
+        //throw new InvalidDataException("Did not expect diagonal line.");
+    }
+}
+
+Console.WriteLine(field.ToString());
+
+var floorTilesFlattened = field.FloorData.Cast<int>();
+
+var numPointsAtLeastTwo = floorTilesFlattened.Count(x => x >= 2);
+
+Console.WriteLine($"Sections with value 2 or more: {numPointsAtLeastTwo}.");
