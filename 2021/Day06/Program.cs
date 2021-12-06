8dc8279401e560bd6dd06df1b77b118da1147b1f
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-06 08:14:16 -0500

    2021 Day 06 Part 2

--- 2021/Day06/Program.cs
+++ 2021/Day06/Program.cs
@@ -8,15 +8,48 @@ var numbers = (await new StringFileReader().ReadInputFromFile())
 // Part 1
 const int NormalCycleStart = 6;
 const int NewFishCycleStart = NormalCycleStart + 2;
-const int NumberOfDays = 80;
+const int PartOneDays = 80;
 
-foreach(var day in Enumerable.Range(0, NumberOfDays))
+void RunFishLifeSimulation(int numberOfDays)
 {
-    var newFish = numbers.Count(x => x == 0);
+    var fishLives = numbers.GroupBy(x => x)
+        .ToDictionary(g => g.Key, g => (long)g.Count());
 
-    numbers = numbers.Select(x => x == 0 ? NormalCycleStart : x - 1)
-        .Concat(Enumerable.Repeat(NewFishCycleStart, newFish))
-        .ToList();
+    foreach (var day in Enumerable.Range(0, numberOfDays))
+    {
+        // Grab the 0 count
+        fishLives.TryGetValue(0, out var newFishThisDay);
+
+        // Evaluate life times
+        for(var i = 1; i < 9; i++)
+        {
+            // Handle the missing keys for the first few days
+            var newCount = fishLives.TryGetValue(i, out var dictCount) ? dictCount : 0;
+
+            fishLives[i - 1] = newCount;
+        }
+
+        // Process the fish that created new ones this cycle
+        fishLives[NormalCycleStart] += newFishThisDay;
+
+        // Add the new fish for this cycle
+        fishLives[NewFishCycleStart] = newFishThisDay;
+
+        //var newFish = numbers.Count(x => x == 0);
+
+        //numbers = numbers.Select(x => x == 0 ? NormalCycleStart : x - 1)
+        //    .Concat(Enumerable.Repeat(NewFishCycleStart, newFishThisDay))
+        //    .ToList();
+    }
+
+    var numberOfFish = fishLives.Select(x => x.Value).Sum();
+
+    Console.WriteLine($"Number of fish after {numberOfDays} days: {numberOfFish}.");
 }
 
-Console.WriteLine($"Number of fish after {NumberOfDays} days: {numbers.Count}.");
+RunFishLifeSimulation(PartOneDays);
+
+// Part 2
+const int PartTwoDays = 256;
+
+RunFishLifeSimulation(PartTwoDays);
