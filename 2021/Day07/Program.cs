65ff155c7b82688767a5acdf4747e33a5b442ce8
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-07 08:42:57 -0500

    2021 Day 07 Part 2

--- 2021/Day07/Program.cs
+++ 2021/Day07/Program.cs
@@ -17,3 +17,44 @@ var bestOption = Enumerable.Range(minPos, maxPos - minPos)
     .First();
 
 Console.WriteLine($"Best position is {bestOption.Position} requiring {bestOption.FuelRequired} fuel.");
+
+// Part 2
+
+// Not sure we really need all this
+// I didn't test just calculating it each time -> Enumerable.Range(0, difference).Sum()
+// I tested it just now, didn't wait around for it to finish
+var fuelRequirements = new Dictionary<int, int>();
+
+int FuelRequired(int currentPosition, int destinationPosition)
+{
+    var difference = Math.Abs(currentPosition - destinationPosition);
+
+    if (difference == 1)
+    {
+        return 1;
+    }
+
+    if (fuelRequirements.ContainsKey(difference))
+    {
+        return fuelRequirements[difference];
+    }
+
+    var positionMove = currentPosition > destinationPosition ? -1 : 1;
+
+    var requiredFuel = FuelRequired(currentPosition + positionMove, destinationPosition) + difference;
+
+    fuelRequirements[difference] = requiredFuel;
+
+    return requiredFuel;
+}
+
+var bestOptionPart2 = Enumerable.Range(minPos, maxPos - minPos)
+    .Select(testPos => new
+    {
+        FuelRequired = horizontalPositions.Select(x => FuelRequired(x, testPos)).Sum(),
+        Position = testPos
+    })
+    .OrderBy(x => x.FuelRequired)
+    .First();
+
+Console.WriteLine($"Best position with scaling fuel is {bestOptionPart2.Position} requiring {bestOptionPart2.FuelRequired} fuel.");
