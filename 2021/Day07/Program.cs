6571c3e4e7bb166be74f3866b9aecc9c94a63496
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-07 08:27:24 -0500

    2021 Day 07 Part 1

---
+++ 2021/Day07/Program.cs
@@ -0,0 +1,19 @@
+﻿using Helpers.FileReaders;
+
+var horizontalPositions = (await new StringFileReader().ReadInputFromFile()).SelectMany(x => x.Split(',')).Select(x => int.Parse(x)).ToList();
+
+var minPos = horizontalPositions.Min();
+var maxPos = horizontalPositions.Max();
+
+// Part 1
+
+var bestOption = Enumerable.Range(minPos, maxPos - minPos)
+    .Select(testPos => new
+    {
+        FuelRequired = horizontalPositions.Select(x => Math.Abs(x - testPos)).Sum(),
+        Position = testPos
+    })
+    .OrderBy(x => x.FuelRequired)
+    .First();
+
+Console.WriteLine($"Best position is {bestOption.Position} requiring {bestOption.FuelRequired} fuel.");
