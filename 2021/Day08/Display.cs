ced0aad97a90a1365efa7f2efe22bab27a94f2cc
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-08 07:16:29 -0500

    2021 Day 08 Part 1

---
+++ 2021/Day08/Display.cs
@@ -0,0 +1,78 @@
+﻿namespace Day08
+{
+    internal class DisplayMetadata
+    {
+        public Display OutputDisplay { get; init; }
+
+        public Segment[] UniqueSignals { get; init; }
+    }
+
+    internal class Display
+    {
+        public Segment[] Segments { get; init; }
+    }
+
+    internal class Segment
+    {
+        public Segment(SignalWire[] signalWires)
+        {
+            EnabledWires = signalWires;
+
+            var attemptedDigit = CorrectSignals.Where(x => x.Value.Length == EnabledWires.Length)
+                .ToList();
+
+            if (attemptedDigit.Count == 1)
+            {
+                DisplayedDigit = attemptedDigit[0].Key;
+            }
+            else
+            {
+                DisplayedDigit = Digit.Unknown;
+            }
+        }
+
+        public SignalWire[] EnabledWires { get; }
+
+        public Digit DisplayedDigit { get; set; }
+
+        public static Dictionary<Digit, SignalWire[]> CorrectSignals = new Dictionary<Digit, SignalWire[]>
+        {
+            { Digit.Zero, new [] { SignalWire.a, SignalWire.b, SignalWire.c, SignalWire.e, SignalWire.f, SignalWire.g } },
+            { Digit.One, new [] { SignalWire.c, SignalWire.f} },
+            { Digit.Two, new [] { SignalWire.a, SignalWire.c, SignalWire.d, SignalWire.e, SignalWire.g } },
+            { Digit.Three, new [] { SignalWire.a, SignalWire.c, SignalWire.d, SignalWire.f, SignalWire.g } },
+            { Digit.Four, new [] { SignalWire.b, SignalWire.c, SignalWire.d, SignalWire.f } },
+            { Digit.Five, new [] { SignalWire.a, SignalWire.b, SignalWire.d, SignalWire.f, SignalWire.g } },
+            { Digit.Six, new [] { SignalWire.a, SignalWire.b, SignalWire.d, SignalWire.e, SignalWire.f, SignalWire.g } },
+            { Digit.Seven, new [] { SignalWire.a, SignalWire.c, SignalWire.f } },
+            { Digit.Eight, new [] { SignalWire.a, SignalWire.b, SignalWire.c, SignalWire.d, SignalWire.e, SignalWire.f, SignalWire.g } },
+            { Digit.Nine, new [] { SignalWire.a, SignalWire.b, SignalWire.c, SignalWire.d, SignalWire.f, SignalWire.g } },
+        };
+    }
+    
+    enum SignalWire
+    {
+        a,
+        b,
+        c,
+        d,
+        e,
+        f,
+        g
+    }
+
+    enum Digit
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Unknown = 10
+    }
+}
---
+++ 2021/Day08/Program.cs
@@ -0,0 +1,44 @@
+﻿using Day08;
+using Helpers;
+
+var inputData = await new SegmentDataReader().ReadInputFromFile();
+
+// Part 1
+var part1Digits = new[] { Digit.One, Digit.Four, Digit.Seven, Digit.Eight };
+var part1Answer = inputData.SelectMany(x => x.OutputDisplay.Segments)
+    .Count(seg => part1Digits.Contains(seg.DisplayedDigit));
+
+Console.WriteLine($"Output with easily recognized digits (1, 4, 7, 8): {part1Answer}");
+
+// Part 2
+
+foreach(var input in inputData)
+{
+
+}
+
+class SegmentDataReader : FileReader<DisplayMetadata>
+{
+    protected override DisplayMetadata ProcessLineOfFile(string line)
+    {
+        var split = line.Split('|');
+
+        var uniqueSignals = split[0].Split(' ')
+            .Select(x => x.ToCharArray().Select(c => Enum.Parse<SignalWire>(c.ToString())).ToArray())
+            .Select(x => new Segment(x))
+            .ToArray();
+
+        var outputSegments = split[1].Split(' ')
+            .Select(x => new Segment(x.ToCharArray().Select(c => Enum.Parse<SignalWire>(c.ToString())).ToArray()))
+            .ToArray();
+
+        return new DisplayMetadata
+        {
+            OutputDisplay = new Display
+            {
+                Segments = outputSegments
+            },
+            UniqueSignals = uniqueSignals
+        };
+    }
+}
