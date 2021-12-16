a306d452b83e8b6ae1d5d9c8d947412fa35c798a
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-15 21:58:23 -0500

    2019 Day 16 Part 2
    Thanks to some old reddit hint threads. In the second half of the input the pattern is just 0...0,1...1. So we are just summing the back half or less of the digits and then doing a mod 10.
    
    In addition, the offset added means we only need to calculate the digits after the offset, anything before it gets zeroed out by the pattern.

--- 2019/PuzzleDays/Day16/Program.cs
+++ 2019/PuzzleDays/Day16/Program.cs
@@ -11,26 +11,6 @@ var patternDictionary = new Dictionary<int, long[]>
                             }
                         };
 
-void LeftShiftArray(long[] array,
-                    int shift)
-{
-    shift %= array.Length;
-    var buffer = new long[shift];
-    Array.Copy(array,
-               buffer,
-               shift);
-    Array.Copy(array,
-               shift,
-               array,
-               0,
-               array.Length - shift);
-    Array.Copy(buffer,
-               0,
-               array,
-               array.Length - shift,
-               shift);
-}
-
 long[] GetPatternForDigit(int digit)
 {
     if (!patternDictionary.TryGetValue(digit,
@@ -85,13 +65,61 @@ List<long> GetDigitsAfterPhases(List<long> startValue,
     return currentValue;
 }
 
-var inputString = "59719811742386712072322509550573967421647565332667367184388997335292349852954113343804787102604664096288440135472284308373326245877593956199225516071210882728614292871131765110416999817460140955856338830118060988497097324334962543389288979535054141495171461720836525090700092901849537843081841755954360811618153200442803197286399570023355821961989595705705045742262477597293974158696594795118783767300148414702347570064139665680516053143032825288231685962359393267461932384683218413483205671636464298057303588424278653449749781937014234119757220011471950196190313903906218080178644004164122665292870495547666700781057929319060171363468213087408071790";
+List<long> GetDigitsAfterPhasesPart2(List<long> startValue,
+                                     long phases,
+                                     int offset)
+{
+    var currentValue = startValue.ToArray();
+
+    for (var phase = 0; phase < phases; phase++)
+    {
+        var newValue = currentValue.ToArray();
+
+        var runningSum = 0L;
+
+        for (var digit = currentValue.Length - 1; digit >= offset; digit--)
+        {
+            runningSum += currentValue[digit];
+
+            var updatedDigit = runningSum % 10;
+
+            newValue[digit] = updatedDigit;
+            //Console.WriteLine($"New value of digit {digit} after {phase} phases: {newValue[digit - 1]}.");
+        }
+
+        currentValue = newValue;
+    }
+
+    return currentValue.ToList();
+}
+
+var inputString = "03036732577212944063491565474664";
 var numberOfPhases = 100;
 
 var inputSignal = inputString.ToCharArray()
                              .Select(x => long.Parse(x.ToString()))
                              .ToList();
 
-var newValue = GetDigitsAfterPhases(inputSignal, numberOfPhases);
+// Part 1
+
+var newValue = GetDigitsAfterPhases(inputSignal,
+                                    numberOfPhases);
 
 Console.WriteLine($"Output signal: {string.Join(string.Empty, newValue).Substring(0, 8)}.");
+
+// Part 2
+
+var numberOfRepeats = 10000;
+
+var messageOffset = long.Parse(string.Join(string.Empty, inputString.Take(7)));
+
+var bigInput = Enumerable.Range(0,
+                                numberOfRepeats)
+                         .SelectMany(x => inputSignal)
+                         .ToList();
+
+var newValuePart2 = GetDigitsAfterPhasesPart2(bigInput,
+                                              numberOfPhases,
+                                              (int)messageOffset);
+
+Console.WriteLine($"Output signal: {string.Join(string.Empty, newValuePart2.Skip(Convert.ToInt32(messageOffset)).Take(8)).Substring(0, 8)}.");
