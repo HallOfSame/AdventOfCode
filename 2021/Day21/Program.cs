0b97d89ca1d83bcfddfac68c22d3ef79be33c0c7
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-21 08:38:38 -0500

    2021 Day 21 Part 2
    Didn't take too long. Missed at first that you needed to track the current turn when caching. Was also only rolling once per turn instead of 3 times which led to super tiny answers at first.

--- 2021/Day21/Program.cs
+++ 2021/Day21/Program.cs
@@ -1,4 +1,5 @@
-﻿using Helpers.Structure;
+﻿using Helpers.Extensions;
+using Helpers.Structure;
 
 var solver = new Solver(new Day21Problem());
 
@@ -87,7 +88,21 @@ class Day21Problem : ProblemBase
 
     protected override Task<string> SolvePartTwoInternal()
     {
-        throw new NotImplementedException();
+        var (p1Win, p2Win) = DiceGame.CountWins(new GameModel
+                                                {
+                                                    PlayerOnePos = playerOneStart,
+                                                    PlayerTwoPos = playerTwoStart,
+                                                    PlayerOneScore = 0,
+                                                    PlayerTwoScore = 0,
+                                                    IsPlayerOnesTurn = true,
+                                                    CurrentTurn = 0
+                                                });
+
+        var higherWin = p1Win > p2Win
+                            ? p1Win
+                            : p2Win;
+
+        return Task.FromResult(higherWin.ToString());
     }
 
     public override Task ReadInput()
@@ -96,5 +111,165 @@ class Day21Problem : ProblemBase
         playerTwoStart = 7;
 
         return Task.CompletedTask;
+    }
+}
+
+class DiceGame
+{
+    private static Dictionary<GameModel, (long playerOne, long playerTwo)> knownWins = new();
+
+    public static (long playerOne, long playerTwo) CountWins(GameModel gameModel)
+    {
+        if (knownWins.TryGetValue(gameModel,
+                                  out var knownWin))
+        {
+            return knownWin;
+        }
+
+        if (gameModel.IsOver)
+        {
+            if (gameModel.PlayerOneWon)
+            {
+                knownWins[gameModel] = (1, 0);
+                return (1, 0);
+            }
+
+            knownWins[gameModel] = (0, 1);
+            return (0, 1);
+        }
+
+        var winCount = (playerOne: 0L, playerTwo: 0L);
+
+        void AddWins(int rollVal)
+        {
+            var newModel = gameModel.Roll(rollVal);
+
+            var (p1, p2) = CountWins(newModel);
+
+            winCount.playerOne += p1;
+            winCount.playerTwo += p2;
+        }
+
+        for (var firstRoll = 1; firstRoll <= 3; firstRoll++)
+        {
+            for (var secondRoll = 1; secondRoll <= 3; secondRoll++)
+            {
+                for (var thirdRoll = 1; thirdRoll <= 3; thirdRoll++)
+                {
+                    AddWins(firstRoll + secondRoll + thirdRoll);
+                }
+            }
+        }
+
+        knownWins[gameModel] = winCount;
+
+        return winCount;
+    }
+}
+
+class GameModel
+{
+    public int PlayerOnePos { get; set; }
+
+    public int PlayerTwoPos { get; set; }
+
+    public int PlayerOneScore { get; set; }
+
+    public int PlayerTwoScore { get; set; }
+
+    public bool IsPlayerOnesTurn { get; set; }
+
+    public int CurrentTurn { get; set; }
+
+    public bool IsOver
+    {
+        get
+        {
+            return PlayerOneScore >= 21 || PlayerTwoScore >= 21;
+        }
+    }
+
+    public bool PlayerOneWon
+    {
+        get
+        {
+            return PlayerOneScore > PlayerTwoScore;
+        }
+    }
+
+    public GameModel Roll(int rollValue)
+    {
+        var clone = new GameModel
+                    {
+                        PlayerOnePos = this.PlayerOnePos,
+                        PlayerTwoPos = this.PlayerTwoPos,
+                        PlayerOneScore = this.PlayerOneScore,
+                        PlayerTwoScore = this.PlayerTwoScore,
+                        IsPlayerOnesTurn = this.IsPlayerOnesTurn,
+                        CurrentTurn = this.CurrentTurn + 1
+                    };
+
+        var currentPos = clone.IsPlayerOnesTurn
+                             ? clone.PlayerOnePos
+                             : clone.PlayerTwoPos;
+
+        currentPos = (currentPos + rollValue) % 10;
+
+        if (currentPos == 0)
+        {
+            currentPos = 10;
+        }
+
+        if (clone.IsPlayerOnesTurn)
+        {
+            clone.PlayerOnePos = currentPos;
+            clone.PlayerOneScore += clone.PlayerOnePos;
+        }
+        else
+        {
+            clone.PlayerTwoPos = currentPos;
+            clone.PlayerTwoScore += clone.PlayerTwoPos;
+        }
+
+        clone.IsPlayerOnesTurn = !clone.IsPlayerOnesTurn;
+
+        return clone;
+    }
+
+    protected bool Equals(GameModel other)
+    {
+        return CurrentTurn == other.CurrentTurn && IsPlayerOnesTurn == other.IsPlayerOnesTurn && PlayerOnePos == other.PlayerOnePos && PlayerTwoPos == other.PlayerTwoPos && PlayerOneScore == other.PlayerOneScore && PlayerTwoScore == other.PlayerTwoScore;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null,
+                            obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this,
+                            obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((GameModel)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(PlayerOnePos,
+                                PlayerTwoPos,
+                                PlayerOneScore,
+                                PlayerTwoScore,
+                                IsPlayerOnesTurn,
+                                CurrentTurn);
     }
 }
