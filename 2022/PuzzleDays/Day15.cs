3793873a85f2d61ad2377461a162bc32f6f5cd7b
Dylan OGara <samuel.ogara94@gmail.com>
2022-12-15 21:53:27 -0500

    2022 Day 15
    Really slow implementation. 2.4 min when parallelized.
    Even part 1 takes 4 seconds.

--- 2022/PuzzleDays/Day15.cs
+++ 2022/PuzzleDays/Day15.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
+using Helpers.Extensions;
 using Helpers.FileReaders;
 using Helpers.Maps;
 using Helpers.Structure;
@@ -30,9 +31,7 @@ namespace PuzzleDays
                     continue;
                 }
 
-                var couldNotHaveBeacon = sensors.Any(x => CoordinateHelper.ManhattanDistance(x,
-                                                                                             testCoordinate)
-                                                          <= x.DistanceToClosestBeacon);
+                var couldNotHaveBeacon = IsInvalidBeaconLocation(testCoordinate);
 
                 if (couldNotHaveBeacon)
                 {
@@ -40,12 +39,101 @@ namespace PuzzleDays
                 }
             }
 
+            if (impossibleBeaconLocations != 5240818)
+            {
+                // For refactoring
+                throw new Exception("You broke it");
+            }
+
             return impossibleBeaconLocations.ToString();
+        }
+
+        private bool IsInvalidBeaconLocation(Coordinate testLocation)
+        {
+            return sensors.Any(x => CoordinateHelper.ManhattanDistance(x,
+                                                                       testLocation)
+                                    <= x.DistanceToClosestBeacon);
         }
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var possibleLocationsPerSensor = sensors.ToList()
+                                                    .AsParallel()
+                                                    .Select((sensor, idx) =>
+                                                            {
+                                                                Console.WriteLine($"Processing sensor number: {idx + 1} of {sensors.Count}");
+                                                                var queue = new Queue<Coordinate>();
+
+                                                                queue.Enqueue(new Coordinate(sensor.X,
+                                                                                             sensor.Y + sensor.DistanceToClosestBeacon));
+
+                                                                queue.Enqueue(new Coordinate(sensor.X, sensor.Y - sensor.DistanceToClosestBeacon));
+
+                                                                queue.Enqueue(new Coordinate(sensor.X + sensor.DistanceToClosestBeacon,
+                                                                                             sensor.Y));
+
+                                                                queue.Enqueue(new Coordinate(sensor.X - sensor.DistanceToClosestBeacon,
+                                                                                             sensor.Y));
+
+                                                                var result = new HashSet<Coordinate>();
+
+                                                                var visited = new HashSet<Coordinate>();
+
+                                                                while (queue.Any())
+                                                                {
+                                                                    var current = queue.Dequeue();
+
+                                                                    visited.Add(current);
+
+                                                                    var neighborsToCheck = current.GetNeighbors(true)
+                                                                                                  .Where(x => x.X is >= 0 and <= 4_000_000 && x.Y is >= 0 and <= 4_000_000 && !visited.Contains(x));
+
+                                                                    foreach (var neighbor in neighborsToCheck)
+                                                                    {
+                                                                        var distanceToSensor = CoordinateHelper.ManhattanDistance(sensor,
+                                                                                                                                  neighbor);
+
+                                                                        var diff = distanceToSensor - sensor.DistanceToClosestBeacon;
+
+                                                                        if (diff == 1)
+                                                                        {
+                                                                            // If it is exactly one outside of the closest, it is a possible result
+                                                                            if (!IsInvalidBeaconLocation(neighbor))
+                                                                            {
+                                                                                result.Add(neighbor);
+                                                                            }
+                                                                        }
+                                                                        else if (diff is 0 or -1)
+                                                                        {
+                                                                            // Otherwise, keep searching if it is around the borders
+                                                                            // Add to visited here to prevent duplicate adds
+                                                                            visited.Add(neighbor);
+                                                                            queue.Enqueue(neighbor);
+                                                                        }
+                                                                    }
+                                                                }
+
+                                                                return result;
+                                                            })
+                                                    .Aggregate((curr,
+                                                                prev) =>
+                                                               {
+                                                                   curr.UnionWith(prev);
+
+                                                                   return curr;
+                                                               });
+
+            var distressBeacon = possibleLocationsPerSensor.First();
+
+            var result = ((distressBeacon.X * 4_000_000m) + distressBeacon.Y);
+
+            if (result != 13213086906101m)
+            {
+                // For refactoring
+                throw new Exception("You broke it");
+            }
+
+            return result.ToString();
         }
 
         public override async Task ReadInput()
