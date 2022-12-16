9dc7cd63de14ccde87173d123892bbbc62aed96a
Dylan OGara <samuel.ogara94@gmail.com>
2022-12-15 19:08:54 -0500

    2022 Day 15 Part One
    A little slow, going to refactor as a test for part 2

---
+++ 2022/PuzzleDays/Day15.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.RegularExpressions;
+
+using Helpers.FileReaders;
+using Helpers.Maps;
+using Helpers.Structure;
+
+namespace PuzzleDays
+{
+    public class Day15 : ProblemBase
+    {
+        protected override async Task<string> SolvePartOneInternal()
+        {
+            var targetRow = 2_000_000;
+
+            var minX = sensors.Min(x => x.X - x.DistanceToClosestBeacon);
+
+            var maxX = sensors.Max(x => x.X + x.DistanceToClosestBeacon);
+
+            Console.WriteLine($"Searching from {minX} to {maxX}");
+
+            var impossibleBeaconLocations = 0;
+
+            for (var i = minX; i <= maxX; i++)
+            {
+                var testCoordinate = new Coordinate(i,
+                                                    targetRow);
+
+                if (beacons.Contains(testCoordinate))
+                {
+                    continue;
+                }
+
+                var couldNotHaveBeacon = sensors.Any(x => CoordinateHelper.ManhattanDistance(x,
+                                                                                             testCoordinate)
+                                                          <= x.DistanceToClosestBeacon);
+
+                if (couldNotHaveBeacon)
+                {
+                    impossibleBeaconLocations++;
+                }
+            }
+
+            return impossibleBeaconLocations.ToString();
+        }
+
+        protected override async Task<string> SolvePartTwoInternal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task ReadInput()
+        {
+            var lines = await new StringFileReader().ReadInputFromFile();
+
+            beacons = new HashSet<Coordinate>();
+            sensors = new HashSet<Sensor>();
+
+            var parseRegex = new Regex("Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)");
+
+            foreach (var line in lines)
+            {
+                var regexMatch = parseRegex.Match(line);
+
+                var sensorCoord = new Coordinate(int.Parse(regexMatch.Groups[1]
+                                                                     .Value),
+                                                 int.Parse(regexMatch.Groups[2]
+                                                                     .Value));
+
+                var beaconCoord = new Coordinate(int.Parse(regexMatch.Groups[3]
+                                                                     .Value),
+                                                 int.Parse(regexMatch.Groups[4]
+                                                                     .Value));
+
+                var distance = CoordinateHelper.ManhattanDistance(beaconCoord,
+                                                                  sensorCoord);
+
+                sensors.Add(new Sensor
+                            {
+                                X = sensorCoord.X,
+                                Y = sensorCoord.Y,
+                                DistanceToClosestBeacon = distance
+                            });
+
+                beacons.Add(beaconCoord);
+            }
+        }
+
+        public HashSet<Coordinate> beacons;
+
+        public HashSet<Sensor> sensors;
+    }
+}
+
+public class Sensor : Coordinate
+{
+    public int DistanceToClosestBeacon { get; init; }
+
+}
---
+++ Libraries/Helpers/Maps/CoordinateHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helpers.Maps
+{
+    public static class CoordinateHelper
+    {
+        #region Class Methods
+
+        public static int ManhattanDistance(Coordinate c1,
+                                            Coordinate c2)
+        {
+            return Math.Abs(c1.X - c2.X) + Math.Abs(c1.Y - c2.Y);
+        }
+
+        #endregion
+    }
+}
