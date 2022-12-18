9ae90b593142a3d9d9b0ba65b243fc3fe7012184
Dylan OGara <samuel.ogara94@gmail.com>
2022-12-18 08:29:19 -0500

    2022 Day 18 Part 1
    Suspiciously easy. Easier than part one of yesterday. This is a bad sign for part 2

---
+++ 2022/PuzzleDays/Day18.cs
@@ -0,0 +1,52 @@
+﻿using Helpers;
+using Helpers.Extensions;
+using Helpers.Maps._3D;
+using Helpers.Structure;
+
+namespace PuzzleDays
+{
+    public class Day18 : ProblemBase
+    {
+        protected override async Task<string> SolvePartOneInternal()
+        {
+            var exposedSides = 0;
+
+            var coordHash = coordinates.ToHashSet();
+
+            foreach (var coordinate in coordinates)
+            {
+                var baseExposedSides = 6;
+
+                var coveredSides = coordinate.GetNeighbors().Count(x => coordHash.Contains(x));
+
+                exposedSides += (baseExposedSides - coveredSides);
+            }
+
+            return exposedSides.ToString();
+        }
+
+        protected override async Task<string> SolvePartTwoInternal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task ReadInput()
+        {
+            coordinates = await new CoordinateReader().ReadInputFromFile();
+        }
+
+        private List<Coordinate3d> coordinates;
+    }
+}
+
+class CoordinateReader : FileReader<Coordinate3d>
+{
+    protected override Coordinate3d ProcessLineOfFile(string line)
+    {
+        var split = line.Split(',');
+
+        return new Coordinate3d(int.Parse(split[0]),
+                                int.Parse(split[1]),
+                                int.Parse(split[2]));
+    }
+}
---
+++ Libraries/Helpers/Extensions/Coordinate3dExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Helpers.Maps._3D;
+
+namespace Helpers.Extensions
+{
+    public static class Coordinate3dExtensions
+    {
+        #region Class Methods
+
+        public static Coordinate3d[] GetNeighbors(this Coordinate3d start,
+                                                  bool includeDiagonals = false)
+        {
+            var startX = start.X;
+            var startY = start.Y;
+            var startZ = start.Z;
+
+            var neighbors = new Coordinate3d[includeDiagonals
+                                                 ? 8
+                                                 : 6];
+
+            // To the left
+            neighbors[0] = new Coordinate3d(startX - 1,
+                                            startY,
+                                            startZ);
+
+            // To the right
+            neighbors[1] = new Coordinate3d(startX + 1,
+                                            startY,
+                                            startZ);
+
+            // In front of
+            neighbors[2] = new Coordinate3d(startX,
+                                            startY + 1,
+                                            startZ);
+
+            // Behind
+            neighbors[3] = new Coordinate3d(startX,
+                                            startY - 1,
+                                            startZ);
+
+            // Above
+            neighbors[4] = new Coordinate3d(startX,
+                                            startY,
+                                            startZ + 1);
+
+            // Below
+            neighbors[5] = new Coordinate3d(startX,
+                                            startY,
+                                            startZ - 1);
+
+            if (includeDiagonals)
+            {
+                throw new InvalidOperationException("Diagonals not coded for 3d yet.");
+            }
+
+            return neighbors;
+        }
+
+        #endregion
+    }
+}
