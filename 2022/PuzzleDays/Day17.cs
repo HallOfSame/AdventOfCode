e7afd7c9e651becfa9706bbd795eaeaeadb4533f
Dylan OGara <samuel.ogara94@gmail.com>
2022-12-18 08:17:59 -0500

    2022 Day 17 Part 2
    The 1 trillion thing makes it pretty obvious you aren't meant to simulate the whole thing. Finding a cycle isn't too hard once you come up with a good hash for it (or at least one that works on the example and your input). The math on the cycle height was a little annoying with adding 1 to get height vs Y coord. Doesn't run super fast, about 4 seconds and change. Could probably improve with a better hashing func.

--- 2022/PuzzleDays/Day17.cs
+++ 2022/PuzzleDays/Day17.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 
 using Helpers.FileReaders;
 using Helpers.Maps;
@@ -171,7 +172,253 @@ namespace PuzzleDays
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var floor = -1;
+
+            var rockIndex = 0;
+
+            var moveDownThisTurn = false;
+
+            var topRockY = -1;
+
+            var filledCoordinates = new HashSet<Coordinate>();
+
+            Rock? currentRock = null;
+
+            var jetIndex = 0;
+
+            var stoppedRocks = 0;
+
+            bool IsValidMove(HashSet<Coordinate> coordinates)
+            {
+                var minX = coordinates.Min(x => x.X);
+                var maxX = coordinates.Max(x => x.X);
+
+                var minY = coordinates.Min(y => y.Y);
+
+                if (minX < 0
+                    || maxX > 6)
+                {
+                    return false;
+                }
+
+                if (minY < 0)
+                {
+                    return false;
+                }
+
+                return coordinates.All(x => !filledCoordinates.Contains(x));
+            }
+
+            // Detect cycle (at rock spawn) based on:
+            // height in each column relative to highest rock
+            // Current rock type being spawned
+            // Index in jet
+            var setupCodes = new Dictionary<string, (int spawnCount, int currentHeight)>();
+
+            string cycleCode;
+
+            while (true)
+            {
+                if (currentRock is null)
+                {
+                    var newRockSpawnCoord = new Coordinate(2,
+                                                           topRockY + 4);
+
+                    currentRock = (Shape)rockIndex switch
+                    {
+                        Shape.Line => new LineRock(),
+                        Shape.Cross => new CrossRock(),
+                        Shape.L => new LRock(),
+                        Shape.VertLine => new VertLineRock(),
+                        Shape.Square => new SquareRock(),
+                        _ => throw new ArgumentException("Unexpected rock index")
+                    };
+
+                    rockIndex++;
+
+                    if (rockIndex == 5)
+                    {
+                        rockIndex = 0;
+                    }
+
+                    currentRock.Create(newRockSpawnCoord);
+
+                    moveDownThisTurn = false;
+
+                    var code = $"{rockIndex},{jetIndex},";
+
+                    for (var i = 0; i < 7; i++)
+                    {
+                        var columnCoordinates = filledCoordinates.Where(x => x.X == i);
+
+                        var columnDelta = (columnCoordinates.Any()
+                                               ? columnCoordinates.Max(x => x.Y)
+                                               : 0)
+                                          - topRockY;
+
+                        code += columnDelta + ",";
+                    }
+
+                    if (setupCodes.ContainsKey(code))
+                    {
+                        cycleCode = code;
+                        break;
+                    }
+
+                    setupCodes.Add(code,
+                                   (stoppedRocks, topRockY));
+
+                    //Draw(filledCoordinates,
+                    //     currentRock.Location);
+                }
+
+                if (!moveDownThisTurn)
+                {
+                    var direction = jetMovement[jetIndex] == '<'
+                                        ? RockDirection.Left
+                                        : RockDirection.Right;
+
+                    jetIndex++;
+
+                    if (jetIndex == jetMovement.Length)
+                    {
+                        jetIndex = 0;
+                    }
+
+                    var updatedLocation = currentRock.GetMoveLocation(direction);
+
+                    if (IsValidMove(updatedLocation))
+                    {
+                        currentRock.Location = updatedLocation;
+                    }
+                }
+                else
+                {
+                    var updatedLocation = currentRock.GetMoveLocation(RockDirection.Down);
+
+                    if (IsValidMove(updatedLocation))
+                    {
+                        currentRock.Location = updatedLocation;
+                    }
+                    else
+                    {
+                        filledCoordinates.UnionWith(currentRock.Location);
+                        topRockY = Math.Max(topRockY,
+                                            currentRock.Location.Max(x => x.Y));
+                        currentRock = null;
+                        stoppedRocks++;
+                    }
+                }
+
+                moveDownThisTurn = !moveDownThisTurn;
+            }
+
+            // Figure out what the cycle is like
+            var cycleInfo = setupCodes[cycleCode];
+
+            // Length == how many rocks spawn in a cycle
+            var cycleLength = stoppedRocks - cycleInfo.spawnCount;
+
+            // Height == how much height is added per cycle
+            var cycleHeight = topRockY - cycleInfo.currentHeight;
+
+            // Calculate how many cycles we need to run to get close to 1 tril
+            var cycles = Math.Floor((1_000_000_000_000m - cycleInfo.spawnCount) / cycleLength);
+
+            // Calculate how much height all those cycles would add
+            var currentHeight = topRockY + 1;
+
+            var heightAfterCycling = (cycles * cycleHeight) + cycleInfo.currentHeight + 1;
+            var topAtCycle = currentHeight;
+
+            // Then figure out how many more rocks need to drop to get to an even 1 tril
+            var remainingCycles = (1_000_000_000_000m - cycleInfo.spawnCount) % cycleLength;
+
+            stoppedRocks = 0;
+            
+            // Repetitive but I'm too lazy to extract a function right now
+            while (true)
+            {
+                if (stoppedRocks == remainingCycles)
+                {
+                    break;
+                }
+
+                if (currentRock is null)
+                {
+                    var newRockSpawnCoord = new Coordinate(2,
+                                                           topRockY + 4);
+
+                    currentRock = (Shape)rockIndex switch
+                    {
+                        Shape.Line => new LineRock(),
+                        Shape.Cross => new CrossRock(),
+                        Shape.L => new LRock(),
+                        Shape.VertLine => new VertLineRock(),
+                        Shape.Square => new SquareRock(),
+                        _ => throw new ArgumentException("Unexpected rock index")
+                    };
+
+                    rockIndex++;
+
+                    if (rockIndex == 5)
+                    {
+                        rockIndex = 0;
+                    }
+
+                    currentRock.Create(newRockSpawnCoord);
+
+                    moveDownThisTurn = false;
+
+                    //Draw(filledCoordinates,
+                    //     currentRock.Location);
+                }
+
+                if (!moveDownThisTurn)
+                {
+                    var direction = jetMovement[jetIndex] == '<'
+                                        ? RockDirection.Left
+                                        : RockDirection.Right;
+
+                    jetIndex++;
+
+                    if (jetIndex == jetMovement.Length)
+                    {
+                        jetIndex = 0;
+                    }
+
+                    var updatedLocation = currentRock.GetMoveLocation(direction);
+
+                    if (IsValidMove(updatedLocation))
+                    {
+                        currentRock.Location = updatedLocation;
+                    }
+                }
+                else
+                {
+                    var updatedLocation = currentRock.GetMoveLocation(RockDirection.Down);
+
+                    if (IsValidMove(updatedLocation))
+                    {
+                        currentRock.Location = updatedLocation;
+                    }
+                    else
+                    {
+                        filledCoordinates.UnionWith(currentRock.Location);
+                        topRockY = Math.Max(topRockY,
+                                            currentRock.Location.Max(x => x.Y));
+                        currentRock = null;
+                        stoppedRocks++;
+                    }
+                }
+
+                moveDownThisTurn = !moveDownThisTurn;
+            }
+
+            // Add our cyclying height to the last bit of simulation we did
+            var result = (topRockY + 1) - topAtCycle + heightAfterCycling;
+
+            return result.ToString();
         }
 
         public override async Task ReadInput()
@@ -218,6 +465,15 @@ abstract class Rock
                                })
                        .ToHashSet();
     }
+}
+
+class Code
+{
+    public int RockIndex { get; set; }
+
+    public int JetIndex { get; set; }
+
+    public string ColumnDeltas { get; set; }
 }
 
 class LineRock : Rock
