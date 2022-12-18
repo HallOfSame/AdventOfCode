a538378d153bf45e751229b8f02188b5d4ec809c
Dylan OGara <samuel.ogara94@gmail.com>
2022-12-18 11:57:38 -0500

    2022 Day 18 Part 2
    Thankfully was not as super hard even though part 1 was easy. Maybe I just got lucky on a good train of though.
    First attempt didn't path find and only expanded in one (X, Y, or Z) direction. Comment on the new code explains why we need to search all paths out. The example input didn't have that edge case.
    
    Even with bugs got a super narrow range from attempted answers which was funny. Could have just entered all 15 or so to get it.

--- 2022/PuzzleDays/Day18.cs
+++ 2022/PuzzleDays/Day18.cs
@@ -1,4 +1,6 @@
-﻿using Helpers;
+﻿using System.Diagnostics;
+
+using Helpers;
 using Helpers.Extensions;
 using Helpers.Maps._3D;
 using Helpers.Structure;
@@ -27,7 +29,176 @@ namespace PuzzleDays
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var boxEdgeSize = 1;
+
+            // Get a bounding box around our known coordinates
+            var xBound = new[]
+                         {
+                             coordinates.Min(x => x.X) - boxEdgeSize,
+                             coordinates.Max(x => x.X) + boxEdgeSize
+                         };
+
+            var yBound = new[]
+                         {
+                             coordinates.Min(x => x.Y) - boxEdgeSize,
+                             coordinates.Max(x => x.Y) + boxEdgeSize
+                         };
+
+            var zBound = new[]
+                         {
+                             coordinates.Min(x => x.Z) - boxEdgeSize,
+                             coordinates.Max(x => x.Z) + boxEdgeSize
+                         };
+
+            var exposedSides = 0;
+
+            var coordHash = coordinates.ToHashSet();
+
+            var internalCoordinates = new HashSet<Coordinate3d>();
+
+            // Non-diagonal directions we could travel from a point
+            var transforms = new[]
+                             {
+                                 new Coordinate3d(-1,
+                                                  0,
+                                                  0),
+                                 new Coordinate3d(1,
+                                                  0,
+                                                  0),
+                                 new Coordinate3d(0,
+                                                  -1,
+                                                  0),
+                                 new Coordinate3d(0,
+                                                  1,
+                                                  0),
+                                 new Coordinate3d(0,
+                                                  0,
+                                                  -1),
+                                 new Coordinate3d(0,
+                                                  0,
+                                                  1),
+                             };
+
+            // For all coordinates in the droplet
+            foreach (var coordinate in coordinates)
+            {
+                var outerUncoveredSides = 0;
+
+                // Get all "neighbors", basically just the 6 directions around this coordinate
+                var neighbors = coordinate.GetNeighbors();
+
+                // From each of those neighbors, see if we can escape the droplet
+                // If we can, this is one exposed face
+                foreach (var startPoint in neighbors)
+                {
+                    // This is a known coordinate in the droplet, side is covered based on part 1 logic
+                    if (coordHash.Contains(startPoint))
+                    {
+                        continue;
+                    }
+
+                    // We already know this coordinate is internal space and can fast skip it
+                    if (internalCoordinates.Contains(startPoint))
+                    {
+                        continue;
+                    }
+
+                    // The main logic to find exposed faces
+                    // Search for a path out of the box
+                    // Sort of a djikstra where we end if coordinates hit a certain range
+                    // For example:
+                    // (endless void of space)
+                    // #  ####
+                    // #    @#
+                    // #######
+                    // The @ here is exposed, so we need to look at any path that gets us through the bounding box
+                    bool CanEscapeBoundingBox()
+                    {
+                        var current = startPoint;
+
+                        var checkQueue = new Queue<Coordinate3d>();
+
+                        // Add the initial directions we could travel
+                        transforms.Select(x => current + x)
+                                  .ToList()
+                                  .ForEach(x => checkQueue.Enqueue(x));
+
+                        // Keep track of where we've been
+                        var visited = new HashSet<Coordinate3d>();
+
+                        while (checkQueue.Any())
+                        {
+                            var updatedPosition = checkQueue.Dequeue();
+
+                            if (visited.Contains(updatedPosition))
+                            {
+                                // Already been here
+                                continue;
+                            }
+
+                            visited.Add(updatedPosition);
+
+                            if (coordHash.Contains(updatedPosition))
+                            {
+                                // This is a known coordinate of the droplet
+                                continue;
+                            }
+
+                            // We already know this coordinate is internal space
+                            if (internalCoordinates.Contains(updatedPosition))
+                            {
+                                continue;
+                            }
+
+                            if (updatedPosition.X < xBound[0]
+                                || updatedPosition.X > xBound[1]
+                                || updatedPosition.Y < yBound[0]
+                                || updatedPosition.Y > yBound[1]
+                                || updatedPosition.Z < zBound[0]
+                                || updatedPosition.Z > zBound[1])
+                            {
+                                // We've escaped the bounds of our known coordinates
+                                return true;
+                            }
+
+                            // Add new directions to check
+                            transforms.Select(x => updatedPosition + x)
+                                      .Where(x => !visited.Contains(x))
+                                      .ToList()
+                                      .ForEach(x => checkQueue.Enqueue(x));
+                        }
+
+                        // Ran out of places to check and never escaped the bounds, this space is internal
+                        return false;
+                    }
+
+                    // Check expanding in other directions
+                    // If we can expand out of the bounding box without a collision this is an outer exposed edge
+                    if (CanEscapeBoundingBox())
+                    {
+                        outerUncoveredSides++;
+                        continue;
+                    }
+
+                    // Track if we know a coordinate is an internal space so we can skip processing it later
+                    internalCoordinates.Add(startPoint);
+                }
+
+                exposedSides += outerUncoveredSides;
+            }
+
+            // Got an oddly narrow range when code still had bugs
+            if (exposedSides <= 2551)
+            {
+                throw new Exception("Too low");
+            }
+
+            if (exposedSides >= 2583)
+            {
+                throw new Exception("Too high");
+            }
+
+            return exposedSides.ToString();
         }
 
         public override async Task ReadInput()
