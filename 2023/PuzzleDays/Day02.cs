dfda0c138bd5b68d754b38cf7f1da47cb530d49b
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-02 09:42:14 -0500

    2023 Day 2 Part 1

---
+++ 2023/PuzzleDays/Day02.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.RegularExpressions;
+using Helpers;
+using Helpers.Structure;
+
+namespace PuzzleDays;
+
+public class Day02 : ProblemBase
+{
+    private List<GameResults> gameResults { get; set; }
+
+    protected override async Task<string> SolvePartOneInternal()
+    {
+        var redLimit = 12;
+        var blueLimit = 14;
+        var greenLimit = 13;
+
+        var result = gameResults.Where(game => game.RevealedSets.All(set => set.RedCount <= redLimit &&
+                                                                            set.BlueCount <= blueLimit &&
+                                                                            set.GreenCount <= greenLimit))
+            .Select(game => game.GameId).Sum();
+
+        return result.ToString();
+    }
+
+    protected override async Task<string> SolvePartTwoInternal()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task ReadInput()
+    {
+        var reader = new GameResultReader();
+
+        gameResults = await reader.ReadInputFromFile();
+    }
+}
+
+internal class GameResults
+{
+    public int GameId { get; set; }
+    public List<CubeSet> RevealedSets { get; set; }
+}
+
+internal class CubeSet
+{
+    public int RedCount { get; set; }
+    public int BlueCount { get; set; }
+    public int GreenCount { get; set; }
+}
+
+internal class GameResultReader : FileReader<GameResults>
+{
+    protected override GameResults ProcessLineOfFile(string line)
+    {
+        var initialSplit = line.Split(':');
+
+        var gameId = int.Parse(initialSplit[0].Replace("Game ", string.Empty));
+
+        var cubeResultSplits = initialSplit[1].Split(';');
+
+        var cubeSets = new List<CubeSet>();
+
+        foreach (var cubeSplit in cubeResultSplits)
+        {
+            var thisCubeShowing = new CubeSet();
+
+            var cubesInThisResult = cubeSplit.Split(',');
+
+            foreach (var match in cubesInThisResult)
+            {
+                var splitMatch = match.Trim().Split(' ');
+                var count = int.Parse(splitMatch[0]);
+                var color = splitMatch[1];
+
+                switch (color)
+                {
+                    case "red":
+                        thisCubeShowing.RedCount = count;
+                        break;
+                    case "blue":
+                        thisCubeShowing.BlueCount = count;
+                        break;
+                    case "green":
+                        thisCubeShowing.GreenCount = count;
+                        break;
+                    default:
+                        throw new Exception($"{color} is not expected");
+                }
+            }
+
+            cubeSets.Add(thisCubeShowing);
+        }
+
+        return new GameResults
+        {
+            GameId = gameId,
+            RevealedSets = cubeSets
+        };
+    }
+}
