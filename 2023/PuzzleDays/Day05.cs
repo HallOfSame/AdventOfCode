9b836d44fcd3fb976d75b2f0ee8796ea20e45d43
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-05 09:57:14 -0500

    2023 Day 5 Part 2
    Hard part 2 for day 5. First time I really felt like I was stuck for a while this year. First thought was just iterating the location and seeing if it mapped back to a valid seed range. But the answer ended up being in the 500M range and that was too slow. Looked on reddit for approaches other people used and saw somebody mention they converted the input ranges to the next stage. That is what this solution is, intuitively not that complex but it was a pain to write.

--- 2023/PuzzleDays/Day05.cs
+++ 2023/PuzzleDays/Day05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,7 +35,139 @@ namespace PuzzleDays
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var seedRanges = new List<Range>();
+
+            // Build the seed ranges
+            for (var i = 0; i < startingSeeds.Count; i += 2)
+            {
+                var rangeStart = startingSeeds[i];
+                var rangeLength = startingSeeds[i + 1];
+
+                seedRanges.Add(new Range
+                {
+                    Start = rangeStart,
+                    Length = rangeLength,
+                });
+            }
+
+            var ranges = seedRanges;
+
+            // Iterate the conversion stages
+            foreach (var converter in converters)
+            {
+                // Break apart the ranges at this stage to all the ranges the could be in the next stage
+                ranges = ranges.SelectMany(x => ConvertRange(x, converter)).ToList();
+            }
+
+            // Then just find the smallest start of a range that we have available at the end
+            return ranges.Min(x => x.Start).ToString();
+        }
+
+        private List<Range> ConvertRange(Range initialRange, Converter converter)
+        {
+            var minValue = initialRange.Start;
+
+            var maxValue = initialRange.End;
+
+            var splitRanges = new List<Range>();
+
+            // Taken from S/O, returns the length of overlap
+            long FindOverlap(Range one, Range two)
+            {
+                return Math.Max(0, Math.Min(one.End, two.End) - Math.Max(one.Start, two.Start) + 1);
+            }
+
+            // First, search the mapping table for the next stage
+            // Find any parts of the initial range that get mapped to different values in the new stage
+            foreach (var map in converter.RangeMaps)
+            {
+                // If this doesn't overlap the input range, just ignore it
+                if (FindOverlap(map.SourceRange, initialRange) == 0)
+                {
+                    continue;
+                }
+
+                // Calculate the split out from the original range that overlaps this mapped range
+                var newRangeStart = Math.Max(minValue, map.SourceRange.Start);
+                var newRangeEnd = Math.Min(maxValue, map.SourceRange.End);
+
+                splitRanges.Add(new Range
+                {
+                    Start = newRangeStart,
+                    Length = newRangeEnd - newRangeStart + 1,
+                });
+            }
+
+            // Put the splits in ascending order so that we can fill in the gaps
+            splitRanges = splitRanges.OrderBy(x => x.Start)
+                .ToList();
+
+            // If no splits, nothing overlaps and we can return the initial range
+            if (!splitRanges.Any())
+            {
+                return new List<Range>
+                {
+                    initialRange
+                };
+            }
+
+            var currentValue = initialRange.Start;
+
+            var splitIndex = 0;
+
+            // Tracking pieces from the input range that don't have a mapping and just use the same value in the destination
+            var untouchedRanges = new List<Range>();
+
+            while (true)
+            {
+                // If our current value is below the next split
+                if (currentValue - splitRanges[splitIndex]
+                        .Start != 0)
+                {
+                    // Add the gap between where we are and our next split
+                    untouchedRanges.Add(new Range
+                    {
+                        Start = currentValue,
+                        Length = splitRanges[splitIndex]
+                            .Start - currentValue,
+                    });
+                }
+
+                // Move to the end of the next split
+                currentValue = splitRanges[splitIndex]
+                    .End + 1;
+                splitIndex++;
+
+                // Stop when we run out of splits
+                if (splitIndex == splitRanges.Count)
+                {
+                    break;
+                }
+            }
+
+            // Add the end if we have any gap remaining
+            if (currentValue < maxValue)
+            {
+                untouchedRanges.Add(new Range
+                {
+                    Start = currentValue,
+                    Length = maxValue - currentValue + 1,
+                });
+            }
+
+            // Now we've split out the sections of the initial range that get mapped to new values
+            // And we've calculated the ranges to fill the gaps in the initial range that don't get mapped
+
+            // Convert the mapped ranges to the new values in the destination
+            var mappedRanges = splitRanges.Select(x => new Range
+            {
+                Start = converter.ConvertValue(x.Start),
+                Length = x.Length,
+            });
+
+            // Then concat everything and return
+            return untouchedRanges.Concat(mappedRanges)
+                .ToList();
         }
 
         public override async Task ReadInput()
@@ -136,8 +269,21 @@ namespace PuzzleDays
 
                 return matchingRange.ConvertToDestinationValue(value);
             }
+
+            public long InverseConvertValue(long value)
+            {
+                var matchingRange = RangeMaps.FirstOrDefault(x => x.DestinationRange.IsNumberWithinRange(value));
+
+                if (matchingRange is null)
+                {
+                    return value;
+                }
+
+                return matchingRange.ConvertToSourceValue(value);
+            }
         }
 
+        [DebuggerDisplay("{SourceRange} {DestinationRange}")]
         class RangeMap
         {
             public Range SourceRange { get; set; }
@@ -148,8 +294,14 @@ namespace PuzzleDays
             {
                 return SourceRange.GetRangeOffset(value) + DestinationRange.Start;
             }
+
+            public long ConvertToSourceValue(long value)
+            {
+                return DestinationRange.GetRangeOffset(value) + SourceRange.Start;
+            }
         }
 
+        [DebuggerDisplay("{Start} - {End} ({Length})")]
         class Range
         {
             public long Start { get; set; }
