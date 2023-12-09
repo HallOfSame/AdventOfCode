d45591023d91379aded090d8c7aed5c6d2f2bb10
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-09 08:04:46 -0500

    2023 Day 9 Part 2
    Shockingly easy, I expected way harder for a Saturday. Maybe the recursive approach was the "smart" way of doing it? But it seems like an obvious way of doing it to me. Whole thing runs in 3 ms for parts 1&2.

--- 2023/PuzzleDays/Day09.cs
+++ 2023/PuzzleDays/Day09.cs
@@ -35,14 +35,39 @@ namespace PuzzleDays
                 diffs[i] = secondValue - firstValue;
             }
 
-            var nextValue = GetNextValue(diffs);
+            var nextValueDiff = GetNextValue(diffs);
 
-            return currentList[^1] + nextValue;
+            return currentList[^1] + nextValueDiff;
         }
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var result = histories.Select(GetPrevValue)
+                .Sum();
+
+            return result.ToString();
+        }
+
+        private int GetPrevValue(int[] currentList)
+        {
+            if (currentList.All(x => x == 0))
+            {
+                return 0;
+            }
+
+            var diffs = new int[currentList.Length - 1];
+
+            for (var i = 0; i < currentList.Length - 1; i++)
+            {
+                var secondValue = currentList[i + 1];
+                var firstValue = currentList[i];
+
+                diffs[i] = secondValue - firstValue;
+            }
+
+            var prevValueDiff = GetPrevValue(diffs);
+
+            return currentList[0] - prevValueDiff;
         }
 
         public override async Task ReadInput()
