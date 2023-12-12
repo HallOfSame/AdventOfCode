0d1d6f622ff971b5f0b615dee2161f301df5e8e5
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-12 08:51:28 -0500

    2023 Day 12 Part 1
    Don't love this solution but it runs in 70ms so it's not like super slow. Feels like there's some optimization around the working with a substring instead of the full thing I could do. But I didn't want to do that before part 2 in case it wasn't needed.

---
+++ 2023/PuzzleDays/Day12.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+using Helpers.Extensions;
+using Helpers.Structure;
+
+namespace PuzzleDays
+{
+    public class Day12 : ProblemBase
+    {
+        protected override async Task<string> SolvePartOneInternal()
+        {
+            var result = records.Select(CalculateArrangements)
+                .Sum();
+
+            return result.ToString();
+        }
+
+        private int CalculateArrangements(Record record)
+        {
+            return CalculateRecursive(record.DamagedRecord, record);
+        }
+
+        private int CalculateRecursive(string partialString, Record record)
+        {
+            var nextQuestionMark = partialString.IndexOf('?');
+
+            if (nextQuestionMark == -1)
+            {
+                // Base case
+                return 1;
+            }
+
+            var usingDot = partialString.ReplaceAt(nextQuestionMark, '.');
+            var usingHash = partialString.ReplaceAt(nextQuestionMark, '#');
+            var total = 0;
+
+            if (IsValid(usingDot, record))
+            {
+                total += CalculateRecursive(usingDot, record);
+            }
+
+            if (IsValid(usingHash, record))
+            {
+                total += CalculateRecursive(usingHash, record);
+            }
+
+            return total;
+        }
+
+        private bool IsValid(string proposedRestoration, Record record)
+        {
+            var currentIndex = 0;
+            var currentGroupSizeLimit = record.DamagedGroupsCount[currentIndex];
+            var currentGroupSize = 0;
+            var failOnHash = false;
+
+            for (var i = 0; i < proposedRestoration.Length; i++)
+            {
+                if (proposedRestoration[i] == '?')
+                {
+                    return true;
+                }
+
+                if (proposedRestoration[i] == '#')
+                {
+                    if (failOnHash)
+                    {
+                        return false;
+                    }
+
+                    currentGroupSize++;
+
+                    if (currentGroupSize > currentGroupSizeLimit)
+                    {
+                        return false;
+                    }
+                }
+
+                if (proposedRestoration[i] == '.' && currentGroupSize > 0)
+                {
+                    if (currentGroupSize != currentGroupSizeLimit)
+                    {
+                        return false;
+                    }
+
+                    currentGroupSize = 0;
+                    currentIndex++;
+
+                    if (currentIndex >= record.DamagedGroupsCount.Count)
+                    {
+                        failOnHash = true;
+                    }
+                    else
+                    {
+                        currentGroupSizeLimit = record.DamagedGroupsCount[currentIndex];
+                    }
+                }
+            }
+
+            if (currentGroupSize > 0 && currentGroupSize != currentGroupSizeLimit)
+            {
+                return false;
+            }
+
+            if (currentGroupSize > 0)
+            {
+                currentIndex++;
+            }
+
+            return currentIndex == record.DamagedGroupsCount.Count;
+        }
+
+        protected override async Task<string> SolvePartTwoInternal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task ReadInput()
+        {
+            records = await new RecordReader().ReadInputFromFile();
+        }
+
+        private List<Record> records;
+
+        class Record
+        {
+            public string DamagedRecord { get; set; }
+
+            public List<int> DamagedGroupsCount { get; set; }
+        }
+
+        class RecordReader : FileReader<Record>
+        {
+            protected override Record ProcessLineOfFile(string line)
+            {
+                var splitLine = line.Split(' ');
+
+                var groups = splitLine[1]
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToList();
+
+                return new Record
+                {
+                    DamagedRecord = splitLine[0],
+                    DamagedGroupsCount = groups,
+                };
+            }
+        }
+    }
+}
---
+++ Libraries/Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Helpers.Extensions;
+
+public static class StringExtensions
+{
+    public static string ReplaceAt(this string input, int index, char newChar)
+    {
+        var arr = input.ToCharArray();
+        arr[index] = newChar;
+        return new string(arr);
+    }
+}
