f9c595ef8ebc927bc89208cfc3a684268e948965
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-13 08:45:21 -0500

    2023 Day 13 Part 2
    Pretty easy. My part 1 solution was set up really well for part 2. Instead of all strings equal being the solution, count the number of mismatches we find when considering the current line. If it ends up being == 1 then we have the part 2 solution.

--- 2023/PuzzleDays/Day13.cs
+++ 2023/PuzzleDays/Day13.cs
@@ -74,9 +74,102 @@ namespace PuzzleDays
             return null;
         }
 
+        private int? GetSmudgeCharReflectionLine(List<string> rowsOrColumns)
+        {
+            for (var possibleFirstIndexAfterReflectLine = 1; possibleFirstIndexAfterReflectLine < rowsOrColumns.Count; possibleFirstIndexAfterReflectLine++)
+            {
+                // Total rows or columns == 10
+                // If first row/col after == idx 4
+                // Then we either check 0,1,2,3 or 4,5,6,7,8,9,10 
+                // Since it's uneven, check 0-3 against 4-7
+                var rowsToCheck = Math.Min(possibleFirstIndexAfterReflectLine,
+                                           rowsOrColumns.Count - possibleFirstIndexAfterReflectLine);
+
+                var mismatchCount = 0;
+
+                // Keeping w/ above example, we have 4 row/col to check
+                // Starting with 3 & 4
+                // Work outwards, 2&5, 1&6, ....
+                for (var i = 0; i < rowsToCheck; i++)
+                {
+                    var bottomRowIndex = possibleFirstIndexAfterReflectLine + i;
+                    var topRowIndex = (possibleFirstIndexAfterReflectLine - 1) - i;
+
+                    var diffCharHere = GetStringDiff(rowsOrColumns[bottomRowIndex], rowsOrColumns[topRowIndex]);
+
+                    mismatchCount += diffCharHere;
+
+                    if (mismatchCount > 1)
+                    {
+                        break;
+                    }
+                }
+
+                // Only one mismatch found on this line, so we could fix it by swapping that char
+                // So this line is the part 2 solution
+                if (mismatchCount == 1)
+                {
+                    // This ends up also being the count of lines above / to the left
+                    return possibleFirstIndexAfterReflectLine;
+                }
+            }
+
+            return null;
+        }
+
+        private int GetStringDiff(string s1, string s2)
+        {
+            // Count mismatched characters in a string
+            var len = s1.Length;
+
+            var foundAMismatch = false;
+
+            for (var x = 0; x < len; x++)
+            {
+                if (s1[x] != s2[x])
+                {
+                    if (foundAMismatch)
+                    {
+                        // We really don't care after 2 or more
+                        return 2;
+                    }
+                    
+                    foundAMismatch = true;
+                }
+            }
+
+            return foundAMismatch ? 1 : 0;
+        }
+
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var result = rowSets.Select((rs, idx) =>
+                {
+                    var horizLine = GetSmudgeCharReflectionLine(rs);
+
+                    if (horizLine is not null)
+                    {
+                        return horizLine * 100;
+                    }
+
+                    var vertLine = GetSmudgeCharReflectionLine(columnSets[idx]);
+
+                    if (vertLine is null)
+                    {
+                        throw new Exception($"Did not find a reflection line for index {idx}");
+                    }
+
+                    return vertLine;
+
+                })
+                .Sum();
+
+            if (result >= 35539)
+            {
+                throw new Exception("Too high");
+            }
+
+            return result.ToString();
         }
 
         private List<List<string>> rowSets { get; set; }
