b34d72e95e17106cbad00ebe4d524b83a5df8ec2
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-14 09:37:14 -0500

    2023 Day 14 Part 2
    A little surprised that actually worked. It's not super fast but I think that's more with how I'm hashing things than anything else. Or something inside of run cycle could be optimized.

--- 2023/PuzzleDays/Day14.cs
+++ 2023/PuzzleDays/Day14.cs
@@ -18,7 +18,7 @@ namespace PuzzleDays
 
         protected override async Task<string> SolvePartOneInternal()
         {
-            var updatedCoordinates = RunCycle(Direction.North, coordinates);
+            var updatedCoordinates = RunCycle(Direction.North, coordinates, out _);
 
             var result = CalculateSupportLoad(updatedCoordinates);
 
@@ -26,14 +26,16 @@ namespace PuzzleDays
         }
 
         private List<CoordinateWithCharacter> RunCycle(Direction direction,
-                                                       List<CoordinateWithCharacter> inputCoordinates)
+                                                       List<CoordinateWithCharacter> inputCoordinates,
+                                                       out int moveCount)
         {
+            moveCount = 0;
             Func<Coordinate, int> maxCoordinateSelector = direction switch
             {
                 Direction.East => c => c.X,
-                Direction.West => c => 0 - c.X,
+                Direction.West => c => Math.Abs(0 - c.X),
                 Direction.North => c => c.Y,
-                Direction.South => c => 0 - c.Y,
+                Direction.South => c => Math.Abs(0 - c.Y),
                 _ => throw new Exception("Not a cardinal direction")
             };
 
@@ -45,32 +47,42 @@ namespace PuzzleDays
             };
 
             var coordinateCopy = inputCoordinates.ToList();
+
             var coordinateMap = coordinateCopy.ToDictionary(x => x.Coordinate, x => x);
 
             var maxDirection = coordinateCopy.Max(x => maxCoordinateSelector(x.Coordinate));
 
-            for (var j = maxDirection; j >= 0; j--)
+            var jStart = direction == Direction.North || direction == Direction.East ? maxDirection : 0;
+            var jEnd = jStart == 0 ? maxDirection : 0;
+            var jChange = jStart == 0 ? 1 : -1;
+            Func<int, bool> jTest = jStart == 0 ? (j) => j <= jEnd : (j) => j >= jEnd;
+
+            for (var j = jStart; jTest(j); j += jChange)
             {
                 var circleRocksToProcess = coordinateCopy.Where(coord => matchingCoordinateSelector(coord.Coordinate, j) && coord.Value == CircularRock);
 
                 foreach (var circleRock in circleRocksToProcess)
                 {
+                    var current = coordinateMap[circleRock.Coordinate];
                     var nextCoordinate = circleRock.Coordinate.GetDirection(direction);
 
                     while (coordinateMap.TryGetValue(nextCoordinate, out var next) && next.Value == EmptySpace)
                     {
-                        var originalCoordinate = circleRock.Coordinate;
-                        circleRock.Coordinate = nextCoordinate;
-                        coordinateMap[nextCoordinate] = circleRock;
-                        coordinateMap[originalCoordinate] = new CoordinateWithCharacter(originalCoordinate)
-                        {
-                            Value = EmptySpace
-                        };
+                        moveCount++;
+                        current.Value = EmptySpace;
+                        coordinateMap[nextCoordinate]
+                            .Value = CircularRock;
 
-                        nextCoordinate = circleRock.Coordinate.GetDirection(direction);
+                        current = coordinateMap[nextCoordinate];
+                        nextCoordinate = nextCoordinate.GetDirection(direction);
                     }
                 }
             }
+
+            //if (direction == Direction.East)
+            //{
+            //    coordinateMap.Keys.Draw(x => coordinateMap[x].Value.ToString(), "o");
+            //}
 
             return coordinateCopy;
         }
@@ -84,7 +96,72 @@ namespace PuzzleDays
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var updatedCoordinates = coordinates.ToList();
+
+            var directions = new List<Direction>
+            {
+                Direction.North,
+                Direction.West,
+                Direction.South,
+                Direction.East,
+            };
+
+            var prevSeen = new Dictionary<string, int>();
+
+            var remainingCycles = 1000000000;
+
+            for (var cycle = 0; cycle < 1000000000; cycle++)
+            {
+                remainingCycles -= 1;
+
+                // Off by one, 0 gives the answer before that loop
+                // I could probably just move this to after we run the cycle
+                if (remainingCycles == -1)
+                {
+                    break;
+                }
+
+                for (var i = 0; i < 4; i++)
+                {
+                    var direction = directions[i];
+
+                    updatedCoordinates = RunCycle(direction, updatedCoordinates, out var _);
+                }
+
+                // Track maps we've seen before
+                var hash = string.Join(string.Empty,
+                                       updatedCoordinates.OrderByDescending(x => x.Coordinate.Y)
+                                           .ThenBy(x => x.Coordinate.X)
+                                           .Select(x => x.Value));
+
+                if (prevSeen.TryGetValue(hash, out var currCount))
+                {
+                    prevSeen[hash] = currCount + 1;
+                }
+                else
+                {
+                    prevSeen[hash] = 1;
+                }
+
+                // If the current count is 3 and everything in the dictionary is 1 or 3
+                // This means we've found the end of a cycle that will loop
+                // Using 3 because 1 or 2 won't trigger at the end of a cycle (most parts of the cycle will have count 1)
+                if (currCount + 1 == 3 && prevSeen.All(x => x.Value is 1 or 3))
+                {
+                    // Remove any starter positions that only occur once before the loop begins
+                    var loopCycles = 1000000000 - prevSeen.Count(x => x.Value == 1);
+
+                    // From that, calculate how many steps into the loop that cycle 1000000000 will occur
+                    var stepsIntoCycleToStop = loopCycles % prevSeen.Count(x => x.Value == 3);
+
+                    // Then update so we run until we hit that point of the loop
+                    remainingCycles = stepsIntoCycleToStop;
+                }
+            }
+
+            var result = CalculateSupportLoad(updatedCoordinates);
+
+            return result.ToString();
         }
 
         private List<CoordinateWithCharacter> coordinates;
--- Libraries/Helpers/Maps/CoordinateHelper.cs
+++ Libraries/Helpers/Maps/CoordinateHelper.cs
@@ -1,42 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Helpers.Maps
+namespace Helpers.Maps;
+
+public static class CoordinateHelper
 {
-    public static class CoordinateHelper
-    {
-        #region Class Methods
+    #region Class Methods
 
-        public static int ManhattanDistance(Coordinate c1,
-                                            Coordinate c2)
+    public static int ManhattanDistance(Coordinate c1,
+                                        Coordinate c2)
+    {
+        return Math.Abs(c1.X - c2.X) + Math.Abs(c1.Y - c2.Y);
+    }
+
+    public static Coordinate GetDirection(this Coordinate c, Direction d)
+    {
+        var updatedCoordinate = new Coordinate(c.X, c.Y);
+
+        switch (d)
         {
-            return Math.Abs(c1.X - c2.X) + Math.Abs(c1.Y - c2.Y);
+            case Direction.North:
+                updatedCoordinate.Y += 1;
+                break;
+            case Direction.South:
+                updatedCoordinate.Y -= 1;
+                break;
+            case Direction.East:
+                updatedCoordinate.X += 1;
+                break;
+            case Direction.West:
+                updatedCoordinate.X -= 1;
+                break;
+            default:
+                throw new NotImplementedException();
         }
 
-        public static Coordinate GetDirection(this Coordinate c, Direction d)
-        {
-            var updatedCoordinate = new Coordinate(c.X, c.Y);
+        return updatedCoordinate;
+    }
 
-            switch (d)
+    public static IEnumerable<Direction> InfiniteDirections(List<Direction> directionOrder)
+    {
+        var currentIndex = 0;
+
+        while (true)
+        {
+            if (currentIndex >= directionOrder.Count)
             {
-                case Direction.North:
-                    updatedCoordinate.Y += 1;
-                    break;
-                case Direction.South:
-                    updatedCoordinate.Y -= 1;
-                    break;
-                case Direction.East:
-                    updatedCoordinate.X += 1;
-                    break;
-                case Direction.West:
-                    updatedCoordinate.X -= 1;
-                    break;
-                default:
-                    throw new NotImplementedException();
+                currentIndex = 0;
             }
 
-            return updatedCoordinate;
-        }
+            yield return directionOrder[currentIndex];
 
-        #endregion
+            currentIndex++;
+        }
     }
+
+    #endregion
 }
