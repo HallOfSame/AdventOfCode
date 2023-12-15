ec6ba91fde7444258503a9cdd7fc28a02d88f96a
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2023-12-15 08:01:08 -0500

    2023 Day 15 Part 2
    Not hard but I like these days with complex instructions and stuff. Some smart overrides to GetHashCode and Equals on the classes we use here make this a breeze to code up

--- 2023/PuzzleDays/Day15.cs
+++ 2023/PuzzleDays/Day15.cs
@@ -20,7 +20,47 @@ namespace PuzzleDays
 
         protected override async Task<string> SolvePartTwoInternal()
         {
-            throw new NotImplementedException();
+            var boxes = Enumerable.Range(0, 256)
+                .Select(x => new LensBox(x))
+                .ToDictionary(x => x.Id, x => x);
+
+            foreach (var command in specialStrings)
+            {
+                if (command.Value.EndsWith('-'))
+                {
+                    // Remove command
+                    var lensLabel = new SpecialString
+                    {
+                        Value = command.Value[..^1],
+                    };
+
+                    var lens = new Lens(lensLabel, 0);
+
+                    var boxId = lensLabel.GetHashCode();
+
+                    boxes[boxId].RemoveLens(lens);
+                }
+                else
+                {
+                    // Add command
+                    var lensSplit = command.Value.Split('=');
+                    var lensLabel = new SpecialString
+                    {
+                        Value = lensSplit[0]
+                    };
+                    var focalPower = int.Parse(lensSplit[1]);
+                    var lens = new Lens(lensLabel, focalPower);
+
+                    var boxId = lensLabel.GetHashCode();
+                    boxes[boxId]
+                        .AddOrUpdateLens(lens);
+                }
+            }
+
+            var result = boxes.Values.Select(x => x.FocusingPower())
+                .Sum();
+
+            return result.ToString();
         }
 
         private List<SpecialString> specialStrings;
@@ -36,6 +76,74 @@ namespace PuzzleDays
                     Value = x
                 })
                 .ToList();
+        }
+
+        class LensBox
+        {
+            public int Id { get; }
+
+            private readonly List<Lens> lensList;
+
+            public LensBox(int id)
+            {
+                Id = id;
+                lensList = new List<Lens>();
+            }
+
+            public void RemoveLens(Lens lensToRemove)
+            {
+                var removed = lensList.Remove(lensToRemove);
+
+                // Console.WriteLine($"Removed {lensToRemove.Label.Value} from box {Id}... Present? {removed}");
+            }
+
+            public void AddOrUpdateLens(Lens lensToAdd)
+            {
+                var existingIndex = lensList.IndexOf(lensToAdd);
+
+                if (existingIndex >= 0)
+                {
+                    // Console.WriteLine($"Overwrite lens {lensToAdd.Label.Value} in box {Id} index {existingIndex} from power {lensList[existingIndex].FocalPower} to {lensToAdd.FocalPower}");
+
+                    lensList[existingIndex] = lensToAdd;
+                    return;
+                }
+
+                // Console.WriteLine($"Added lens {lensToAdd.Label.Value} in box {Id} with {lensToAdd.FocalPower} to end of list");
+
+                lensList.Add(lensToAdd);
+            }
+
+            public int FocusingPower()
+            {
+                var boxModifier = Id + 1;
+
+                var lensPowers = lensList.Select((lens, idx) => lens.FocalPower * (idx + 1) * boxModifier);
+
+                return lensPowers.Sum();
+            }
+        }
+
+        class Lens
+        {
+            public SpecialString Label { get; }
+            public int FocalPower { get; }
+
+            public Lens(SpecialString label, int focalPower)
+            {
+                Label = label;
+                FocalPower = focalPower;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return ((Lens)obj).Label.Value == this.Label.Value;
+            }
+
+            public override int GetHashCode()
+            {
+                return Label.GetHashCode();
+            }
         }
 
         class SpecialString
