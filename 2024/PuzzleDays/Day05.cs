211baaa7b56d818ce8df0d9e10b1ac2006216a4d
Dylan <samuel.ogara94@gmail.com>
2024-12-05 17:16:36 -0500

    2024 Day 05 Part 2
    Pretty cool implementation. Use topological sort to figure out the valid ordering of the messed up updates

--- 2024/PuzzleDays/Day05.cs
+++ 2024/PuzzleDays/Day05.cs
@@ -48,24 +48,30 @@ namespace PuzzleDays
             return new ExecState(updates, rules);
         }
 
+        private List<int> invalidUpdateIndices = [];
+
         protected override async Task<string> ExecutePuzzlePartOne()
         {
+            invalidUpdateIndices.Clear();
             var result = 0;
 
-            foreach(var update in InitialState.Updates)
+            for (var index = 0; index < this.InitialState.Updates.Count; index++)
             {
+                var update = this.InitialState.Updates[index];
                 var valid = true;
 
-                foreach(var rule in InitialState.Rules)
+                foreach (var rule in this.InitialState.Rules)
                 {
-                    var beforeIndex = Array.IndexOf(update, rule.BeforePage);
+                    var beforeIndex = Array.IndexOf(update,
+                                                    rule.BeforePage);
                     if (beforeIndex == -1)
                     {
                         // Not valid for this update
                         continue;
                     }
 
-                    var afterIndex = Array.IndexOf(update, rule.AfterPage);
+                    var afterIndex = Array.IndexOf(update,
+                                                   rule.AfterPage);
                     if (afterIndex == -1)
                     {
                         // Not valid for this update
@@ -83,6 +89,11 @@ namespace PuzzleDays
                 {
                     result += update[update.Length / 2];
                 }
+                else
+                {
+                    // For part 2
+                    invalidUpdateIndices.Add(index);
+                }
             }
 
             return result.ToString();
@@ -90,7 +101,72 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            if (invalidUpdateIndices.Count == 0)
+            {
+                throw new InvalidOperationException("Run part one first");
+            }
+
+            var result = 0;
+
+            foreach(var brokenUpdateIndex in invalidUpdateIndices)
+            {
+                var updateToFix = InitialState.Updates[brokenUpdateIndex];
+
+                var orderedPages = GetOrderOfPages(updateToFix.ToHashSet());
+
+                result += orderedPages[orderedPages.Count / 2];
+            }
+
+            return result.ToString();
+        }
+
+        private List<int> GetOrderOfPages(HashSet<int> validPages)
+        {
+            var allPageNumbers = this.InitialState.Rules.SelectMany(x => new [] {x.BeforePage, x.AfterPage }).Where(validPages.Contains).Distinct().ToList();
+            var pagesRequiredBefore = new Dictionary<int, HashSet<int>>();
+
+            foreach(var rule in this.InitialState.Rules)
+            {
+                if (!validPages.Contains(rule.AfterPage) || !validPages.Contains(rule.BeforePage))
+                {
+                    continue;
+                }
+
+                if (pagesRequiredBefore.TryGetValue(rule.AfterPage, out var existingSet))
+                {
+                    existingSet.Add(rule.BeforePage);
+                }
+                else
+                {
+                    pagesRequiredBefore[rule.AfterPage] = [rule.BeforePage];
+                }
+            }
+
+            // Create a sorted order of all elements
+            var orderedPages = new List<int>();
+            // These are the pages that can come before any others
+            var startNodes = allPageNumbers.Where(x => !pagesRequiredBefore.ContainsKey(x)).ToList();
+
+            while(startNodes.Count > 0) 
+            {
+                var next = startNodes.First();
+                startNodes.RemoveAt(0);
+
+                orderedPages.Add(next);
+
+                var pagesThatMustGoAfterNext = pagesRequiredBefore.Where(x => x.Value.Contains(next)).ToList();
+
+                foreach(var neighborNode in pagesThatMustGoAfterNext)
+                {
+                    neighborNode.Value.Remove(next);
+                    if (neighborNode.Value.Count == 0)
+                    {
+                        startNodes.Add(neighborNode.Key);
+                    }
+                }
+            }
+
+            return orderedPages;
         }
     }
 }
