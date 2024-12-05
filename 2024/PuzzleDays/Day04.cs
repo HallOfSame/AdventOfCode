ed4f198694213832ecc76fa08082db6159747c05
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-04 20:44:57 -0500

    2024 Day 4 Part 2
    Pretty easy day. Maybe like 20ms runtime per part. All the coordinate helpers made it quick

--- 2024/PuzzleDays/Day04.cs
+++ 2024/PuzzleDays/Day04.cs
@@ -39,7 +39,23 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            var aCharactersToStartFrom = InitialState.Input.Where(x => x.Value == 'A');
+            coordinatesContainingXmas = [];
+            var total = 0;
+
+            foreach (var startPoint in aCharactersToStartFrom)
+            {
+                var found = FindMasInXFromStartPoint(startPoint.Key);
+
+                if (found.Count > 0)
+                {
+                    total++;
+                }
+
+                coordinatesContainingXmas.UnionWith(found);
+            }
+
+            return total.ToString();
         }
 
         private (int foundCount, HashSet<Coordinate> foundLoc) FindXmasFromStartPoint(Coordinate start)
@@ -80,9 +96,39 @@ namespace PuzzleDays
             return (foundCount, coordinatesInXmas);
         }
 
+        private HashSet<Coordinate> FindMasInXFromStartPoint(Coordinate start)
+        {
+            var coordinatesInXmas = new HashSet<Coordinate>();
+
+            char[] diagOne = [CharAtCoord(start.Move(Direction.NorthWest)), CharAtCoord(start.Move(Direction.SouthEast))];
+            char[] diagTwo = [CharAtCoord(start.Move(Direction.NorthEast)), CharAtCoord(start.Move(Direction.SouthWest))];
+
+            if (IsValidHalf(diagOne) && IsValidHalf(diagTwo))
+            {
+                // I don't really feel like re-arranging this to add em all
+                // So I won't unless I need it to debug
+                // It just returns this for drawing the output
+                coordinatesInXmas.Add(start);
+            }
+
+            return coordinatesInXmas;
+
+            bool IsValidHalf(char[] diag)
+            {
+                return (diag[0] == 'M' && diag[1] == 'S') || (diag[0] == 'S' && diag[1] == 'M');
+            }
+        }
+
         private bool CoordinateIsChar(Coordinate coordinate, char testChar)
         {
             return InitialState.Input.TryGetValue(coordinate, out var actualChar) && actualChar == testChar;
+        }
+
+        private char CharAtCoord(Coordinate coordinate)
+        {
+            // Idk what the default for char is, but it won't match what we test on
+            InitialState.Input.TryGetValue(coordinate, out var actualChar);
+            return actualChar;
         }
 
         public DrawableCoordinate[] GetCoordinates()
