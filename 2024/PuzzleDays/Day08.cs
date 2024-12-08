00ae5be6410a31327c2142b93dedb07cf9eca696
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-08 08:46:14 -0500

    2024 Day 8 Part 2
    Not much to say, pretty easy. Just do what we did for part one but keep going until we leave the map in both directions.

--- 2024/PuzzleDays/Day08.cs
+++ 2024/PuzzleDays/Day08.cs
@@ -9,7 +9,7 @@ namespace PuzzleDays
 {
     public class Day08 : SingleExecutionPuzzle<Day08.ExecState>, IVisualize2d
     {
-        public record ExecState(Dictionary<Coordinate, char> Map, HashSet<Coordinate> Antinodes);
+        public record ExecState(Dictionary<Coordinate, char> Map, HashSet<Coordinate> AntiNodes, HashSet<Coordinate> AntiNodesPart2);
 
         public override PuzzleInfo Info => new(2024, 8, "Resonant Collinearity");
 
@@ -17,12 +17,23 @@ namespace PuzzleDays
         {
             var grid = await new GridFileReader().ReadFromString(puzzleInput);
 
-            return new ExecState(grid.ToDictionary(x => x.Coordinate, x => x.Value), []);
+            return new ExecState(grid.ToDictionary(x => x.Coordinate, x => x.Value), [], []);
         }
 
         protected override async Task<string> ExecutePuzzlePartOne()
         {
-            InitialState.Antinodes.Clear();
+            return CalculateAntiNodes(InitialState.AntiNodes, true).ToString();
+        }
+
+        protected override async Task<string> ExecutePuzzlePartTwo()
+        {
+            return CalculateAntiNodes(InitialState.AntiNodesPart2, false)
+                .ToString();
+        }
+
+        private int CalculateAntiNodes(HashSet<Coordinate> results, bool partOne)
+        {
+            results.Clear();
 
             var frequencies = InitialState.Map.Where(x => x.Value != '.')
                 .GroupBy(x => x.Value)
@@ -38,31 +49,44 @@ namespace PuzzleDays
                     var p1 = pair.First();
                     var p2 = pair.Last();
 
+                    if (!partOne)
+                    {
+                        // The antennas are obviously in line with themselves
+                        results.Add(p1.Key);
+                        results.Add(p2.Key);
+                    }
+
                     var d1 = GetDistanceBetweenCoordinates(p1.Key, p2.Key);
                     var d2 = GetDistanceBetweenCoordinates(p2.Key, p1.Key);
 
                     var a1 = new Coordinate(p1.Key.X - d1.xDist, p1.Key.Y - d1.yDist);
                     var a2 = new Coordinate(p2.Key.X - d2.xDist, p2.Key.Y - d2.yDist);
 
-                    if (InitialState.Map.ContainsKey(a1))
+                    do
                     {
-                        InitialState.Antinodes.Add(a1);
-                    }
 
-                    if (InitialState.Map.ContainsKey(a2))
-                    {
-                        InitialState.Antinodes.Add(a2);
-                    }
+                        if (InitialState.Map.ContainsKey(a1))
+                        {
+                            results.Add(a1);
+                        }
+
+                        if (InitialState.Map.ContainsKey(a2))
+                        {
+                            results.Add(a2);
+                        }
+
+                        if (partOne)
+                        {
+                            break;
+                        }
+
+                        a1 = new Coordinate(a1.X - d1.xDist, a1.Y - d1.yDist);
+                        a2 = new Coordinate(a2.X - d2.xDist, a2.Y - d2.yDist);
+                    } while (InitialState.Map.ContainsKey(a1) || InitialState.Map.ContainsKey(a2));
                 }
             }
 
-            return InitialState.Antinodes.Count
-                .ToString();
-        }
-
-        protected override async Task<string> ExecutePuzzlePartTwo()
-        {
-            throw new NotImplementedException();
+            return results.Count;
         }
 
         private static (decimal xDist, decimal yDist) GetDistanceBetweenCoordinates(Coordinate c1, Coordinate c2)
@@ -74,12 +98,16 @@ namespace PuzzleDays
 
         public DrawableCoordinate[] GetCoordinates()
         {
+            var hashToUse = InitialState.AntiNodesPart2.Count > 0
+                ? InitialState.AntiNodesPart2
+                : InitialState.AntiNodes;
+
             var draw = InitialState.Map.Select(x => new DrawableCoordinate
                 {
                     X = x.Key.X,
                     Y = x.Key.Y,
-                    Color = InitialState.Antinodes.Contains(x.Key) ? "red" : null,
-                    Text = InitialState.Antinodes.Contains(x.Key) && x.Value == '.' ? "#" : x.Value.ToString()
+                    Color = hashToUse.Contains(x.Key) ? "red" : null,
+                    Text = hashToUse.Contains(x.Key) && x.Value == '.' ? "#" : x.Value.ToString()
                 })
                 .ToArray();
             return draw;
