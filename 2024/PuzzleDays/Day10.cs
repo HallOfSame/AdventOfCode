023113451f82efb509cf4849053ec17fe4ec3bf0
Dylan <samuel.ogara94@gmail.com>
2024-12-10 15:22:46 -0500

    2024 Day 10 Part 2
    Pretty easy day. The second part just changes to DFS. The path being directional makes finding neighbors easy

--- 2024/PuzzleDays/Day10.cs
+++ 2024/PuzzleDays/Day10.cs
@@ -37,7 +37,16 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            var trailheads = InitialState.Map.Where(x => x.Value == 0).ToList();
+
+            var totalRating = 0;
+
+            foreach (var trailhead in trailheads)
+            {
+                totalRating += GetTrailheadRating(trailhead.Key);
+            }
+
+            return totalRating.ToString();
         }
 
         private int GetTrailheadScore(Coordinate start)
@@ -70,6 +79,32 @@ namespace PuzzleDays
             }
 
             return score;
+        }
+
+        private int GetTrailheadRating(Coordinate current)
+        {
+            var currentValue = InitialState.Map[current]!.Value;
+
+            if (currentValue == 9)
+            {
+                return 1;
+            }
+
+            var validNeighbors = current.GetNeighbors().Where(x => InitialState.Map.TryGetValue(x, out var neighborVal) && neighborVal == currentValue + 1).ToList();
+
+            if (validNeighbors.Count == 0)
+            {
+                return 0;
+            }
+
+            var pathsFromHere = 0;
+
+            foreach(var neighbor in validNeighbors)
+            {
+                pathsFromHere += GetTrailheadRating(neighbor);
+            }
+
+            return pathsFromHere;
         }
     }
 }
