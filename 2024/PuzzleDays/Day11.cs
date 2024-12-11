3ea5bde6a4653ac2bc0e8eb6e5f0313a9c77097d
Dylan <samuel.ogara94@gmail.com>
2024-12-11 15:32:01 -0500

    2024 Day 11 Part 2
    A little less fun when you know the answer from past years

--- 2024/PuzzleDays/Day11.cs
+++ 2024/PuzzleDays/Day11.cs
@@ -1,21 +1,22 @@
 ﻿using Helpers.Interfaces;
 using Helpers.Structure;
+using System.Globalization;
 
 namespace PuzzleDays
 {
     public class Day11 : StepExecutionPuzzle<Day11.ExecState>, IVisualizeText
     {
-        public record ExecState(List<Stone> Stones, int BlinkCount);
+        public record ExecState(List<Stone> Stones, int BlinkCount, Dictionary<decimal, decimal> PartTwoStoneCounts);
 
         public override PuzzleInfo Info => new(2024, 11, "Plutonian Pebbles");
 
-        public override bool ResetOnNewPart => false;
+        public override bool ResetOnNewPart => true;
 
         protected override async Task<ExecState> LoadInitialState(string puzzleInput)
         {
             var split = puzzleInput.Trim().Split(" ");
             var stones = split.Select(x => new Stone { Number = decimal.Parse(x) }).ToList();
-            return new ExecState(stones, 0);
+            return new ExecState(stones, 0, stones.GroupBy(x => x.Number).ToDictionary(x => x.Key, x => (decimal)x.Count()));
         }
 
         protected override async Task<(bool isComplete, string? result)> ExecutePuzzleStepPartOne()
@@ -27,19 +28,69 @@ namespace PuzzleDays
                 newStones.AddRange(stone.RunRules());
             }
 
-            CurrentState = new ExecState(newStones, CurrentState.BlinkCount + 1);
+            CurrentState = new ExecState(newStones, CurrentState.BlinkCount + 1, CurrentState.PartTwoStoneCounts);
 
             return (CurrentState.BlinkCount == 25, newStones.Count.ToString());
         }
 
         protected override async Task<(bool isComplete, string? result)> ExecutePuzzleStepPartTwo()
         {
-            throw new NotImplementedException();
+            var newStones = new Dictionary<decimal, decimal>();
+
+            foreach(var (stoneNumber, multiplier) in CurrentState.PartTwoStoneCounts)
+            {
+                var numbersToAdd = GetStoneResult(stoneNumber);
+
+                foreach(var number in numbersToAdd)
+                {
+                    if (newStones.TryGetValue(number, out var existingCount))
+                    {
+                        newStones[number] = existingCount + multiplier;
+                    }
+                    else
+                    {
+                        newStones[number] = multiplier;
+                    }
+                }
+            }
+
+            CurrentState = new ExecState(CurrentState.Stones, CurrentState.BlinkCount + 1, newStones);
+
+            return (CurrentState.BlinkCount == 75, newStones.Values.Sum().ToString(CultureInfo.InvariantCulture));
         }
 
         public string GetText()
         {
             return string.Join(" ", CurrentState.Stones.Select(x => x.Number));
+        }
+
+        private decimal[] GetStoneResult(decimal stoneNumber)
+        {
+            if (stoneNumber == 0)
+            {
+                return
+                [
+                    1
+                ];
+            }
+
+            var numberString = stoneNumber.ToString(CultureInfo.InvariantCulture);
+
+            if (numberString.Length % 2 == 0)
+            {
+                var midpoint = numberString.Length / 2;
+
+                return
+                [
+                    decimal.Parse(numberString[..midpoint]),
+                    decimal.Parse(numberString[midpoint..])
+                ];
+            }
+
+            return
+            [
+                stoneNumber * 2024m
+            ];
         }
 
         public class Stone
