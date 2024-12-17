68964ab94e418b182ad3f2eaa7d8915849b346be
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-16 21:38:17 -0500

    2024 Day 16 Part 2
    Pretty hard part 2. Probably had like 3 different attempts before something worked.
    
    1. Try the part 1 algorithm but just keep going as long as we are still finding a path forward less than the part 1 answer. Had issues where we skip visited paths which is necessary to finish the second path. But leaving out the visited in any way just opens up too many long winding searches in the wrong direction (4 GB memory on the real input).
    
    2. Try adding a block along each coordinate in the first short path and then see if we can find another path with the same cost. Closer but there are cases where just one new wall isn't enough to find a third path around the same few coordinates.
    
    3. Read the entire dijkstra wikipedia page. Treat every combination of direction + location as it's own node. Track not just A previous node that has the shortest path to current but every previous node. Then once we are done, we can simply walk backwards along those paths and add all the coordinates to a hashset.
    
    Part 2 actually runs faster than part 1 :)

--- 2024/PuzzleDays/Day16.cs
+++ 2024/PuzzleDays/Day16.cs
@@ -1,11 +1,13 @@
-﻿using Helpers.FileReaders;
+﻿using Helpers.Drawing;
+using Helpers.FileReaders;
 using Helpers.Heaps;
+using Helpers.Interfaces;
 using Helpers.Maps;
 using Helpers.Structure;
 
 namespace PuzzleDays
 {
-    public class Day16 : SingleExecutionPuzzle<Day16.ExecState>
+    public class Day16 : SingleExecutionPuzzle<Day16.ExecState>, IVisualize2d
     {
         public record ExecState(Dictionary<Coordinate, char> Map);
 
@@ -62,7 +64,115 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            bestPathCoordinates = [];
+            var x = DjikstraSearchAllBestPaths();
+            return x.ToString();
+        }
+
+        private int DjikstraSearchAllBestPaths()
+        {
+            // Djikstra search
+            // Since we can turn, and it affects the distance, the nodes of the graph are location + direction currently faced
+            // So that we can model for example that facing north on 1,1 is a 1 to move to 1, 2 but facing east it is 1001, and south would be 2001
+            var distance = new Dictionary<(Coordinate location, Direction facing), int>();
+            var prev = new Dictionary<(Coordinate location, Direction facing), HashSet<(Coordinate location, Direction facing)>>();
+            var priorityQueue = MinHeap.CreateMinHeap<(Coordinate location, Direction facing)>();
+            var start = InitialState.Map.Single(x => x.Value == 'S')
+                .Key;
+
+            distance[(start, Direction.East)] = 0;
+            priorityQueue.Enqueue((start, Direction.East), 0);
+
+            while (priorityQueue.Count != 0)
+            {
+                // Get next node to check
+                var next = priorityQueue.Dequeue();
+
+                // Get all neighbors
+                (Coordinate location, Direction facing, int cost)[] neighbors =
+                [
+                    (next.location.GetDirection(next.facing), next.facing, 1),
+                    (next.location, next.facing.TurnLeft90(), 1000),
+                    (next.location, next.facing.TurnRight90(), 1000),
+                ];
+
+                // Skipping straight neighbor if it is a wall
+                var skip = InitialState.Map[neighbors[0].location] == '#' ? 1 : 0;
+
+                foreach (var neighbor in neighbors.Skip(skip))
+                {
+                    // Calc the distance it took us to get here
+                    var calculatedDistance = distance[next] + neighbor.cost;
+
+                    // If it isn't better, keep going
+                    var existingDistance = distance.GetValueOrDefault((neighbor.location, neighbor.facing), int.MaxValue);
+                    if (calculatedDistance > existingDistance)
+                    {
+                        continue;
+                    }
+
+                    if (calculatedDistance == existingDistance)
+                    {
+                        // We found another valid way here
+                        if (!prev.TryGetValue((neighbor.location, neighbor.facing), out var existingSet))
+                        {
+                            existingSet = [];
+                        }
+
+                        existingSet.Add(next);
+                        continue;
+                    }
+
+                    // This way is guaranteed better than any we found before
+                    // Update our trackers and enqueue
+                    prev[(neighbor.location, neighbor.facing)] = [next];
+                    distance[(neighbor.location, neighbor.facing)] = calculatedDistance;
+                    priorityQueue.Enqueue((neighbor.location, neighbor.facing), calculatedDistance);
+                }
+            }
+
+            var end = InitialState.Map.Single(x => x.Value == 'E')
+                .Key;
+            // Find best distance to the end
+            var bestDistance = distance.Where(x => x.Key.location == end)
+                .Min(x => x.Value);
+            // Then get any location + direction that can meet that
+            var validEndStates = distance.Where(x => x.Key.location == end && x.Value == bestDistance)
+                .Select(x => x.Key)
+                .ToList();
+            
+            // Now we just need to recreate the list of paths
+            bestPathCoordinates.Add(end);
+            var queue = new Queue<(Coordinate location, Direction facing)>();
+            validEndStates.ForEach(queue.Enqueue);
+
+            while (queue.Count != 0)
+            {
+                var next = queue.Dequeue();
+
+                if (InitialState.Map[next.location] == 'S')
+                {
+                    continue;
+                }
+
+                var backtrackStates = prev[next];
+
+                foreach (var state in backtrackStates)
+                {
+                    bestPathCoordinates.Add(state.location); 
+                    queue.Enqueue(state);
+                }
+            }
+
+            return bestPathCoordinates.Count;
+        }
+
+        private HashSet<Coordinate> bestPathCoordinates = [];
+
+        public DrawableCoordinate[] GetCoordinates()
+        {
+            return InitialState.Map.ToDictionary(x => x.Key, x => bestPathCoordinates.Contains(x.Key) ? 'O' : x.Value)
+                .ToDrawableCoordinates(x => bestPathCoordinates.Contains(x) ? "green" : null);
         }
     }
 }
