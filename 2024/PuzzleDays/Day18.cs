de63aef873f1dc5e5bee25977f1b1dadbb3be041
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-18 20:13:40 -0500

    2024 Day 18 Part 2
    Pretty easy day (thankfully, I stayed up way too late last night working on this). Basically just drop bytes until we drop one on our current path. Then re-run dijkstra to see if a path exists. Loop until we can't find a path. Only took about 500 ms on the real input (from my laptop, which is noticeably slower than my PC. On day 16 I did part 1 on my laptop (10s runtime) and part 2 on the desktop (3s runtime for the same part).

--- 2024/PuzzleDays/Day18.cs
+++ 2024/PuzzleDays/Day18.cs
@@ -108,7 +108,98 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            // We handled the first few in part one
+            var nextToFallIndex = InitialState.BytesToDrop;
+
+            var currentPath = path.ToHashSet();
+
+            while (true)
+            {
+                for (; nextToFallIndex < InitialState.FallingBytes.Count; nextToFallIndex++)
+                {
+                    var nextByte = InitialState.FallingBytes[nextToFallIndex];
+                    map[nextByte] = '#';
+
+                    // Drop bytes until one was part of our path
+                    if (currentPath.Contains(nextByte))
+                    {
+                        break;
+                    }
+                }
+
+                // Then try to find a new path
+                currentPath = FindPathToEnd();
+
+                if (currentPath.Count == 0)
+                {
+                    // Empty return means no path
+                    break;
+                }
+            }
+
+            return InitialState.FallingBytes[nextToFallIndex]
+                .ToString();
+        }
+
+        private HashSet<Coordinate> FindPathToEnd()
+        {
+            var start = new Coordinate(0, 0);
+            var target = new Coordinate(InitialState.GridSize - 1, InitialState.GridSize - 1);
+            var distance = new Dictionary<Coordinate, int>();
+            var prev = new Dictionary<Coordinate, Coordinate>();
+            var priorityQueue = MinHeap.CreateMinHeap<Coordinate>();
+
+            distance[start] = 0;
+            priorityQueue.Enqueue(start, 0);
+            var foundPath = false;
+
+            while (priorityQueue.Count != 0)
+            {
+                var next = priorityQueue.Dequeue();
+
+                if (next == target)
+                {
+                    foundPath = true;
+                    break;
+                }
+
+                var neighborsToCheck = next.GetNeighbors()
+                    .Where(x => map.TryGetValue(x, out var atNeighbor) && atNeighbor != '#')
+                    .ToArray();
+
+                foreach (var neighbor in neighborsToCheck)
+                {
+                    var calculatedDistance = distance[next] + 1;
+
+                    var existingDistance = distance.GetValueOrDefault(neighbor, int.MaxValue);
+                    if (calculatedDistance >= existingDistance)
+                    {
+                        continue;
+                    }
+
+                    prev[neighbor] = next;
+                    distance[neighbor] = calculatedDistance;
+                    priorityQueue.Enqueue(neighbor, calculatedDistance);
+                }
+            }
+
+            if (!foundPath)
+            {
+                return [];
+            }
+
+            var pathBuild = target;
+            var newPath = new HashSet<Coordinate>();
+
+            do
+            {
+                newPath.Add(pathBuild);
+                pathBuild = prev[pathBuild];
+            } while (pathBuild != start);
+
+            newPath.Add(start);
+
+            return newPath;
         }
 
         public DrawableCoordinate[] GetCoordinates()
