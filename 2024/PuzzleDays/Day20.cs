1158e05e00805b5dca1c05d637e90a5d659a6774
Dylan <samuel.ogara94@gmail.com>
2024-12-20 13:51:17 -0500

    2024 Day 20 Part 2
    A little harder but a lot of that was due to the problem being unclear. A cheat can just "start" moving in a regular path. Then I had to fix the logic I copied from part two to work better with arbitrary lengths.

--- 2024/PuzzleDays/Day20.cs
+++ 2024/PuzzleDays/Day20.cs
@@ -44,8 +44,6 @@ namespace PuzzleDays
 
                 foreach (var wall in wallsToCheatInto)
                 {
-                    var cheatStart = wall;
-
                     var endPoints = wall.GetNeighbors()
                         .Where(x => x != stepOnPath && (InitialState.Map.IsCharAtCoordinate(x, '.') || x == end))
                         .ToList();
@@ -96,7 +94,63 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            var end = InitialState.Map.First(x => x.Value == 'E')
+                .Key;
+            var start = InitialState.Map.First(x => x.Value == 'S')
+                .Key;
+            var (distanceToEnd, fastestPathNoCheat) = FindPath(end, start);
+            var fastestTimeNoCheat = distanceToEnd[start];
+            var result = 0;
+            
+            var j = new Dictionary<int, int>();
+            var checkedCheats = new HashSet<(Coordinate start, Coordinate end)>();
+
+            // -2 because there is no point checking starting at the end
+            for (var i = 0; i < fastestPathNoCheat.Count - 2; i++)
+            {
+                var stepOnPath = fastestPathNoCheat[i];
+                var cheatStart = stepOnPath;
+
+                // You can activate a cheat and keep moving on a path apparently
+                var endPoints = FindEndpointsForCheat(cheatStart, 20);
+
+                foreach (var (cheatEnd, addedSteps) in endPoints)
+                {
+                    if (checkedCheats.Contains((cheatStart, cheatEnd)))
+                    {
+                        continue;
+                    }
+
+                    var pathAfterCheat = distanceToEnd[cheatEnd];
+
+                    if (pathAfterCheat == int.MaxValue)
+                    {
+                        // Did not come up in the example or real input
+                        throw new InvalidOperationException("We haven't discovered this path yet");
+                    }
+
+                    var pathLengthWithCheat = pathAfterCheat + addedSteps + i;
+                    var savings = fastestTimeNoCheat - pathLengthWithCheat;
+
+                    if (savings <= 0)
+                    {
+                        // Could probably find a way to stop pathfinding this early
+                        continue;
+                    }
+
+                    j.TryGetValue(savings, out var count);
+                    j[savings] = count + 1;
+
+                    if (savings >= 100)
+                    {
+                        result++;
+                    }
+
+                    checkedCheats.Add((cheatStart, cheatEnd));
+                }
+            }
+
+            return result.ToString();
         }
 
         private (Dictionary<Coordinate, int>, List<Coordinate>) FindPath(Coordinate start, Coordinate target)
@@ -156,6 +210,42 @@ namespace PuzzleDays
             newPath.Add(start);
 
             return (distance, newPath);
+        }
+
+        private HashSet<(Coordinate end, int addedSteps)> FindEndpointsForCheat(Coordinate currentPosition, int movesLeft)
+        {
+            var visited = new HashSet<Coordinate>()
+            {
+                currentPosition
+            };
+            var queue = new Queue<(Coordinate position, int movesLeft)>();
+            queue.Enqueue((currentPosition, movesLeft));
+            var validEndpoints = new HashSet<(Coordinate end, int addedSteps)>();
+
+            while(queue.Count > 0)
+            {
+                var (next, movesRemaining) = queue.Dequeue();
+
+                if (InitialState.Map[next] == '.' || InitialState.Map[next] == 'E')
+                {
+                    validEndpoints.Add((next, movesLeft - movesRemaining));
+                }
+
+                if (movesRemaining == 0)
+                {
+                    continue;
+                }
+
+                var neighbors = next.GetNeighbors().Where(x => !visited.Contains(x) && InitialState.Map.ContainsKey(x)).ToList();
+
+                foreach(var neighbor in neighbors)
+                {
+                    visited.Add(neighbor);
+                    queue.Enqueue((neighbor, movesRemaining - 1));
+                }
+            }
+
+            return validEndpoints;
         }
 
         public DrawableCoordinate[] GetCoordinates()
