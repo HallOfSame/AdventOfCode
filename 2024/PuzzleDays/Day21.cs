cc304ae3fdb20e5e4c748e657b828bb9cf67452f
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-22 08:43:42 -0500

    2024 Day 21 Part 2
    Takes some inspection of the example to see the pattern. If robot 0 is controlling the numeric keypad and 1 is controlling the first directional. At robot X+1 doing repeated sequence on robot X runs the same moves over and over. So you can recurse and memoize to prevent the huge amount of possibilities showing up.

--- 2024/PuzzleDays/Day21.cs
+++ 2024/PuzzleDays/Day21.cs
@@ -112,7 +112,94 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
+            partTwoMemo.Clear();
+            var numberOfCenterRobots = 24;
+            var finalRobotKeypadMoves = GetAllNumericKeypadMoves();
+            var directionalKeypadMoves = GetAllDirectionalKeypadMoves();
+
+            var finalRobotPointer = 'A';
+            var result = 0L;
+            foreach (var sequence in InitialState.Sequences)
+            {
+                var minMoves = 0L;
+
+                // The way this works is:
+                // Functionally at each level, the sequence to get the next robot to do something, i.e. <<^A does not change
+                // And it even works deeper down then that, because we have to hit A at the end. So the << of that pattern is the same sequence twice at the next level
+                // So we figure out the directional sequence needed by the robot directly controlling the final one (pretty easy)
+                // And so if we memoize as we go, we quickly get the best way at each level to run a small subsequence of moves
+                foreach (var sequenceChar in sequence)
+                {
+                    var firstKeypadOptions = finalRobotKeypadMoves[(finalRobotPointer, sequenceChar)];
+
+                    var optionToUse = firstKeypadOptions.Select(x => FindMinMovesForDirectionSequence(x,
+                                                                     0,
+                                                                     numberOfCenterRobots,
+                                                                     directionalKeypadMoves))
+                        .Min();
+
+                    minMoves += optionToUse;
+
+                    finalRobotPointer = sequenceChar;
+                }
+
+                var numericPartOfSequence = int.Parse(sequence.Replace("A", string.Empty));
+                result += minMoves * numericPartOfSequence;
+            }
+
+            return result.ToString();
+        }
+
+        private Dictionary<(string sequence, int recursionDepth), long> partTwoMemo = [];
+
+        private long FindMinMovesForDirectionSequence(string directionSequence,
+                                                      int recursionDepth,
+                                                      int maxRecursionDepth,
+                                                      Dictionary<(char start, char end), List<string>> directionalKeypadMoves)
+        {
+            if (partTwoMemo.TryGetValue((directionSequence, recursionDepth), out var existingResult))
+            {
+                return existingResult;
+            }
+
+            var result = 0L;
+            var nextPointer = 'A';
+            foreach (var directionButton in directionSequence)
+            {
+                // If the button is the same, we just hit A again
+                if (directionButton == nextPointer)
+                {
+                    result += 1;
+                    continue;
+                }
+
+                // Figure out options we could use to direct the robot below to hit this button
+                var moveOptionsForThisButton = directionalKeypadMoves[(nextPointer, directionButton)];
+
+                if (recursionDepth == maxRecursionDepth)
+                {
+                    // If we're all the way at the end, then we just need the length of the strings
+                    var optionToUse = moveOptionsForThisButton.MinBy(x => x.Length)!;
+
+                    result += optionToUse.Length;
+                }
+                else
+                {
+                    // Otherwise, recurse to figure out the min at the next level
+                    var optionToUse = moveOptionsForThisButton.Select(x => FindMinMovesForDirectionSequence(x,
+                                                                           recursionDepth + 1,
+                                                                           maxRecursionDepth,
+                                                                           directionalKeypadMoves))
+                        .Min();
+
+                    result += optionToUse;
+                }
+
+                nextPointer = directionButton;
+            }
+
+            partTwoMemo[(directionSequence, recursionDepth)] = result;
+            return result;
         }
 
         private static void ConsolidateOptions(List<List<string>> robotTwoMoveOptions, int index, string current, List<string> results)
