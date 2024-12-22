b76c3a0c0845b57eac84f4e3311b9f3cfdb66416
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-22 17:28:31 -0500

    2024 Day 22 Part 2
    Or not. Not too complex. It's not elegant, takes about 22s to run on the real input (on laptop), but it is pretty straightforward.

--- 2024/PuzzleDays/Day22.cs
+++ 2024/PuzzleDays/Day22.cs
@@ -24,7 +24,7 @@ namespace PuzzleDays
 
             foreach (var startingNumber in InitialState.SecretNumbers)
             {
-                result += CalculateSecretNumber(startingNumber, 2000);
+                result += CalculateSecretNumberAfterRounds(startingNumber, 2000).Last();
             }
 
             return result.ToString();
@@ -32,21 +32,82 @@ namespace PuzzleDays
 
         protected override async Task<string> ExecutePuzzlePartTwo()
         {
-            throw new NotImplementedException();
-        }
+            var allMonkeyLookups = new List<Dictionary<(int, int, int, int), int>>();
 
-        private static long CalculateSecretNumber(long secretNumber, int numberOfRounds)
-        {
-            for (var round = 1; round <= numberOfRounds; round++)
+            foreach (var startingNumber in InitialState.SecretNumbers)
             {
-                var firstMix = secretNumber * 64L;
-                secretNumber = MixAndPrune(secretNumber, firstMix);
-                var secondMix = secretNumber / 32L;
-                secretNumber = MixAndPrune(secretNumber, secondMix);
-                var thirdMix = secretNumber * 2048L;
-                secretNumber = MixAndPrune(secretNumber, thirdMix);
+                // Get the list of secret numbers
+                var secretNumbersForThisMonkey = CalculateSecretNumberAfterRounds(startingNumber, 2000);
+                // Convert them to prices
+                var pricesForMonkey = secretNumbersForThisMonkey.Select(x => (int)(x % 10))
+                    .ToList();
+                // And then price changes
+                var priceChanges = pricesForMonkey.Skip(1)
+                    .Select((curr, idx) => curr - pricesForMonkey[idx])
+                    .ToList();
+
+                // Map out the price the first team each sequence is found
+                var sequencePrices = new Dictionary<(int, int, int, int), int>();
+
+                for (var i = 3; i < priceChanges.Count; i++)
+                {
+                    var sequence = (priceChanges[i - 3], priceChanges[i - 2], priceChanges[i - 1], priceChanges[i]);
+                    // prices list is 1 longer than change list
+                    var priceAtThisTime = pricesForMonkey[i + 1];
+
+                    // Only the first time counts
+                    sequencePrices.TryAdd(sequence, priceAtThisTime);
+                }
+
+                allMonkeyLookups.Add(sequencePrices);
             }
 
+            var bestTotal = 0;
+            var bestSequence = (0, 0, 0, 0);
+            // Just check every sequence that exists and find the best one
+            var allSequencesToCheck = allMonkeyLookups.SelectMany(x => x.Keys)
+                .ToHashSet();
+
+            foreach (var sequenceToTest in allSequencesToCheck)
+            {
+                var totalForThisSequence = allMonkeyLookups.Sum(monkey => monkey.GetValueOrDefault(sequenceToTest, 0));
+
+                if (totalForThisSequence <= bestTotal)
+                {
+                    continue;
+                }
+
+                bestTotal = totalForThisSequence;
+                bestSequence = sequenceToTest;
+            }
+
+            return bestTotal.ToString();
+        }
+        
+        private static List<long> CalculateSecretNumberAfterRounds(long secretNumber, int numberOfRounds)
+        {
+            var secretNumbers = new List<long>
+            {
+                secretNumber
+            };
+
+            for (var round = 1; round <= numberOfRounds; round++)
+            {
+                secretNumber = CalculateSecretNumber(secretNumber);
+                secretNumbers.Add(secretNumber);
+            }
+
+            return secretNumbers;
+        }
+
+        private static long CalculateSecretNumber(long secretNumber)
+        {
+            var firstMix = secretNumber * 64L;
+            secretNumber = MixAndPrune(secretNumber, firstMix);
+            var secondMix = secretNumber / 32L;
+            secretNumber = MixAndPrune(secretNumber, secondMix);
+            var thirdMix = secretNumber * 2048L;
+            secretNumber = MixAndPrune(secretNumber, thirdMix);
             return secretNumber;
         }
 
