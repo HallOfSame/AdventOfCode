ad4b883d3d05d45bf87fb79bf6104bac32cea851
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2024-12-22 16:58:07 -0500

    2024 Day 22 Part 1
    Following simple instructions. Which means part 2 is gonna be scary

---
+++ 2024/PuzzleDays/Day22.cs
@@ -0,0 +1,60 @@
+﻿using Helpers.Structure;
+using InputStorageDatabase;
+
+namespace PuzzleDays
+{
+    public class Day22 : SingleExecutionPuzzle<Day22.ExecState>
+    {
+        public record ExecState(List<int> SecretNumbers);
+
+        public override PuzzleInfo Info => new(2024, 22, "Monkey Market");
+        protected override async Task<ExecState> LoadInputState(string puzzleInput, PuzzleInputType inputType)
+        {
+            var numbers = puzzleInput.Trim()
+                .Split('\n')
+                .Select(int.Parse)
+                .ToList();
+
+            return new ExecState(numbers);
+        }
+
+        protected override async Task<string> ExecutePuzzlePartOne()
+        {
+            var result = 0L;
+
+            foreach (var startingNumber in InitialState.SecretNumbers)
+            {
+                result += CalculateSecretNumber(startingNumber, 2000);
+            }
+
+            return result.ToString();
+        }
+
+        protected override async Task<string> ExecutePuzzlePartTwo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static long CalculateSecretNumber(long secretNumber, int numberOfRounds)
+        {
+            for (var round = 1; round <= numberOfRounds; round++)
+            {
+                var firstMix = secretNumber * 64L;
+                secretNumber = MixAndPrune(secretNumber, firstMix);
+                var secondMix = secretNumber / 32L;
+                secretNumber = MixAndPrune(secretNumber, secondMix);
+                var thirdMix = secretNumber * 2048L;
+                secretNumber = MixAndPrune(secretNumber, thirdMix);
+            }
+
+            return secretNumber;
+        }
+
+        private static long MixAndPrune(long secretNumber, long mixValue)
+        {
+            var mixed = secretNumber ^ mixValue;
+            var pruned = mixed % 16777216;
+            return pruned;
+        }
+    }
+}
