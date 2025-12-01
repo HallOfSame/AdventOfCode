ca7a0536e1029426d98be4cb673684b59b6941a9
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-11-30 22:29:11 -0500

    UI project to try out Avalonia

---
+++ 2025/AoCRunner/App.axaml.cs
@@ -0,0 +1,50 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
+using System.Linq;
+using Avalonia.Markup.Xaml;
+using AoCRunner.ViewModels;
+using AoCRunner.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AoCRunner;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddServices();
+        var provider = serviceCollection.BuildServiceProvider();
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Avoid duplicate validations from both Avalonia and the CommunityToolkit.
+            DisableAvaloniaDataAnnotationValidation();
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainWindowViewModel>()
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void DisableAvaloniaDataAnnotationValidation()
+    {
+        // Get an array of plugins to remove
+        var dataValidationPluginsToRemove =
+            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+        // remove each entry found
+        foreach (var plugin in dataValidationPluginsToRemove)
+        {
+            BindingPlugins.DataValidators.Remove(plugin);
+        }
+    }
+}
---
+++ 2025/AoCRunner/IServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AoCRunner.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+// ReSharper disable InconsistentNaming
+
+namespace AoCRunner
+{
+    internal static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+
+            return services;
+        }
+    }
+}
