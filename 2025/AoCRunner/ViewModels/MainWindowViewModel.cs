17f1f4ed2bf514b15047a70a39a0295836a29b15
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-12-01 22:04:32 -0500

    2025 Day 1 Part 1
    Working with a lazy UI at the moment

--- 2025/AoCRunner/ViewModels/MainWindowViewModel.cs
+++ 2025/AoCRunner/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace AoCRunner.ViewModels;
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using InputStorageDatabase;
+using PuzzleDays;
+
+namespace AoCRunner.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string Input { get; set; } = string.Empty;
+
+    [ObservableProperty]
+    private string _result = string.Empty;
+
+    public Day01 Puzzle = new();
+
+    public async Task RunPartOne()
+    {
+        await Puzzle.LoadInput(Input, PuzzleInputType.Example);
+        var result = await Puzzle.ExecutePartOne();
+            
+        Result = $"Got result {result.Result} in {result.ElapsedTime}";
+    }
+
+    public async Task RunPartTwo()
+    {
+        await Puzzle.LoadInput(Input, PuzzleInputType.Example);
+        var result = await Puzzle.ExecutePartTwo();
+
+        Result = $"Got result {result.Result} in {result.ElapsedTime}";
+    }
 }
---
+++ 2025/PuzzleDays/Day01.cs
@@ -0,0 +1,95 @@
+﻿using Helpers.Structure;
+using InputStorageDatabase;
+
+namespace PuzzleDays;
+
+public class Day01 : SingleExecutionPuzzle<Day01.State>
+{
+    public override PuzzleInfo Info => new(2025, 01, "Secret Entrance");
+    
+    protected override Task<string> ExecutePuzzlePartOne()
+    {
+        var answer = 0;
+
+        foreach (var instruction in InitialState.Instructions)
+        {
+            // To simplify, ignore when we fully rotate during a move
+            var length = instruction.Length % 100;
+
+            var change = instruction.Direction switch
+            {
+                SpinDirection.Right => length,
+                SpinDirection.Left => length * -1,
+                _ => throw new InvalidOperationException("Invalid direction")
+            };
+
+            var newPosition = InitialState.Location + change;
+
+            newPosition = newPosition switch
+            {
+                // Handle turning left past 0 but subtracting (newPosition is negative here) from 100
+                < 0 => 100 + newPosition,
+                // Mod 100 if we turned right from 99
+                > 99 => newPosition % 100,
+                // Otherwise this is just the right value
+                _ => newPosition
+            };
+
+            if (newPosition == 0)
+            {
+                answer++;
+            }
+
+            InitialState.Location = newPosition;
+        }
+
+        return Task.FromResult(answer.ToString());
+    }
+
+    protected override async Task<string> ExecutePuzzlePartTwo()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<State> LoadInputState(string puzzleInput, PuzzleInputType inputType)
+    {
+        var lines = puzzleInput.Trim().Split('\n');
+
+        var instructions = new List<SpinInstruction>();
+
+        foreach (var line in lines)
+        {
+            var direction = line[0] switch
+            {
+                'L' => SpinDirection.Left,
+                'R' => SpinDirection.Right,
+                _ => throw new InvalidOperationException($"{line[0]} is not a valid direction")
+            };
+
+            var distance = int.Parse(line[1..]);
+
+            instructions.Add(new SpinInstruction(direction, distance));
+        }
+
+        return Task.FromResult(new State
+        {
+            Instructions = instructions,
+            Location = 50
+        });
+    }
+
+    public class State
+    {
+        public required int Location { get; set; }
+
+        public required List<SpinInstruction> Instructions { get; set; }
+    }
+
+    public record SpinInstruction(SpinDirection Direction, int Length);
+
+    public enum SpinDirection
+    {
+        Left,
+        Right
+    };
+}
