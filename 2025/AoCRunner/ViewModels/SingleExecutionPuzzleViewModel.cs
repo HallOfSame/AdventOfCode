0186810ddd35e40a68eb0026f51ef460775533fa
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-12-02 23:16:14 -0500

    Better error handling

--- 2025/AoCRunner/ViewModels/SingleExecutionPuzzleViewModel.cs
+++ 2025/AoCRunner/ViewModels/SingleExecutionPuzzleViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Helpers.Interfaces;
 using InputStorageDatabase;
 using System.Threading.Tasks;
@@ -7,24 +9,65 @@ namespace AoCRunner.ViewModels;
 
 public partial class SingleExecutionPuzzleViewModel(ISingleExecutionPuzzle puzzle) : ViewModelBase
 {
+    public string Title => $"Day {puzzle.Info.Day} - {puzzle.Info.Name}";
     public string Input { get; set; } = string.Empty;
 
     [ObservableProperty]
-    private string _result = string.Empty;
+    private string result = string.Empty;
 
     public async Task RunPartOne()
     {
-        await puzzle.LoadInput(Input, PuzzleInputType.Example);
-        var result = await puzzle.ExecutePartOne();
+        if (!await TryCatchInputLoad())
+        {
+            return;
+        }
 
-        Result = $"Got result {result.Result} in {result.ElapsedTime}";
+        var executionResult = await puzzle.ExecutePartOne();
+        SetResultText(executionResult);
     }
 
     public async Task RunPartTwo()
     {
-        await puzzle.LoadInput(Input, PuzzleInputType.Example);
-        var result = await puzzle.ExecutePartTwo();
+        if (!await TryCatchInputLoad())
+        {
+            return;
+        }
 
-        Result = $"Got result {result.Result} in {result.ElapsedTime}";
+        var executionResult = await puzzle.ExecutePartTwo();
+        SetResultText(executionResult);
+    }
+
+    private void SetResultText(ExecutionResult executionResult)
+    {
+        var resultText = new StringBuilder();
+
+        if (executionResult.Exception is not null)
+        {
+            resultText.Append($"Execution threw {executionResult.Exception}");
+        }
+        else
+        {
+            resultText.Append($"Calculated result {executionResult.Result}");
+        }
+
+        // TODO eventually show this on its own property
+        resultText.Append($" in {executionResult.ElapsedTime}");
+
+        Result = resultText.ToString();
+    }
+
+    private async Task<bool> TryCatchInputLoad()
+    {
+        try
+        {
+            await puzzle.LoadInput(Input, PuzzleInputType.Example);
+        }
+        catch (Exception ex)
+        {
+            Result = $"Failed to load input: {ex}";
+            return false;
+        }
+
+        return true;
     }
 }
