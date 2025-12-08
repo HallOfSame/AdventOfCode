06b41dc003200fcc04942556fbd5ea362531ef2e
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-12-07 22:27:53 -0500

    2025 Day 3 Part 2
    It really wasn't that hard but it took me forever. I started with Dfs but longer and that didn't work. Memoizing it didn't help either. Then I tried working backwards in the string replacing digits but that has too many edge cases. Finally came up with this solution which is the simplest answer when you think about it. Just always picking the highest available option for the next digit, because that'll always be higher than anything else we can do

--- 2025/PuzzleDays/Day03.cs
+++ 2025/PuzzleDays/Day03.cs
@@ -51,7 +51,56 @@ public class Day03(ProgressLogger logger) : SingleExecutionPuzzle<Day03.State>
 
     protected override async Task<string> ExecutePuzzlePartTwo()
     {
-        throw new NotImplementedException();
+        var total = 0L;
+
+        foreach (var bank in InitialState.BatteryBanks)
+        {
+            var best = FindBestPartTwo(bank);
+
+            logger.LogProgress($"Best for {bank}: {best}");
+
+            total += best;
+        }
+
+        return total.ToString();
+    }
+
+    private readonly List<char> digitsInOrder = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+
+    private long FindBestPartTwo(string currentBank)
+    {
+        var digits = new List<int>(12);
+        var upperBound = 0;
+
+        // The highest number is always going to be the best leading digit we can make
+        // And we can just work down constraining the string as we go
+        for (var i = 12; i > 0; i--)
+        {
+            var requiredLengthRemaining = 12 - digits.Count;
+            var stringToSearch = currentBank[upperBound..];
+            // This is the latest digit we can pick that still lets us finish a bank
+            var indexBound = stringToSearch.Length - requiredLengthRemaining;
+
+            // Just brute forcing each digit in order
+            foreach (var digitOption in digitsInOrder)
+            {
+                var indexOfOption = stringToSearch.IndexOf(digitOption);
+                // If found and leaves enough to finish the rest, this is the best option to use here
+                if (indexOfOption < 0 || indexOfOption > indexBound)
+                {
+                    continue;
+                }
+
+                // Add this digit
+                digits.Add(int.Parse(digitOption.ToString()));
+                // The upper bound is now the index after this
+                // We + upperBound to keep it relative to the overall string, string indexOfOption is relative to the partial string
+                upperBound = indexOfOption + 1 + upperBound;
+                break;
+            }
+        }
+
+        return long.Parse(string.Join(string.Empty, digits));
     }
 
     protected override async Task<State> LoadInputState(string puzzleInput, PuzzleInputType inputType)
