008e8907d29b0cc2faaa14a19f8b8c0b926c46e5
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-12-10 22:15:28 -0500

    Finishing up some UI and making day 4 a step execution

--- 2025/PuzzleDays/Day04.cs
+++ 2025/PuzzleDays/Day04.cs
@@ -8,7 +8,7 @@ using Helpers.Structure;
 namespace PuzzleDays;
 
 // TODO make a step execution from this
-public class Day04 : SingleExecutionPuzzle<Day04.State>, IVisualize2d
+public class Day04 : StepExecutionPuzzle<Day04.State>, IVisualize2d
 {
     public override PuzzleInfo Info => new(2025, 4, "Printing Department");
 
@@ -16,45 +16,19 @@ public class Day04 : SingleExecutionPuzzle<Day04.State>, IVisualize2d
     {
         return
         [
-            .. InitialState.Grid.Select(x => new DrawableCoordinate
+            .. CurrentState.Grid.Select(x => new DrawableCoordinate
             {
                 Color = "White",
-                Text = x.Value.ToString(),
+                Text = CurrentState.RemovedThisRound.Contains(x.Key) ? "x" : x.Value.ToString(),
                 X = x.Key.X,
                 Y = x.Key.Y
             })
         ];
     }
+    
+    public override bool ResetOnNewPart => false;
 
-    protected override async Task<string> ExecutePuzzlePartOne()
-    {
-        return InitialState.Grid
-            .Count(x => x.Value == '@' && x.Key.GetNeighbors(true).Count(n => InitialState.Grid.TryGetValue(n, out var nChar) && nChar == '@') < 4).ToString();
-    }
-
-    protected override async Task<string> ExecutePuzzlePartTwo()
-    {
-        var removedCount = 0;
-
-        while(true)
-        {
-            var toRemove = InitialState.Grid
-            .Where(x => x.Value == '@' && x.Key.GetNeighbors(true).Count(n => InitialState.Grid.TryGetValue(n, out var nChar) && nChar == '@') < 4).ToList();
-
-            if (toRemove.Count == 0)
-            {
-                break;
-            }
-
-            removedCount += toRemove.Count;
-
-            toRemove.ForEach(x => InitialState.Grid[x.Key] = '.');
-        }
-
-        return removedCount.ToString();
-    }
-
-    protected override async Task<State> LoadInputState(string puzzleInput, PuzzleInputType inputType)
+    protected override async Task<State> LoadInitialState(string puzzleInput)
     {
         var gridData = await new GridFileReader().ReadFromString(puzzleInput);
 
@@ -64,8 +38,35 @@ public class Day04 : SingleExecutionPuzzle<Day04.State>, IVisualize2d
         };
     }
 
+    protected override async Task<(bool isComplete, string? result)> ExecutePuzzleStepPartOne()
+    {
+        var count = InitialState.Grid
+            .Count(x => x.Value == '@' && x.Key.GetNeighbors(true).Count(n => InitialState.Grid.TryGetValue(n, out var nChar) && nChar == '@') < 4).ToString();
+
+        return (true, count);
+    }
+
+    protected override async Task<(bool isComplete, string? result)> ExecutePuzzleStepPartTwo()
+    {
+        var toRemove = CurrentState.Grid
+            .Where(x => x.Value == '@' && x.Key.GetNeighbors(true)
+                       .Count(n => CurrentState.Grid.TryGetValue(n, out var nChar) && nChar == '@') < 4)
+            .ToList();
+
+        CurrentState.RemovedThisRound = toRemove.Select(x => x.Key)
+            .ToHashSet();
+
+        toRemove.ForEach(x => CurrentState.Grid[x.Key] = '.');
+
+        CurrentState.RemovedCount += toRemove.Count;
+
+        return (toRemove.Count == 0, CurrentState.RemovedCount.ToString());
+    }
+
     public class State
     {
         public required Dictionary<Coordinate, char> Grid { get; set; }
+        public int RemovedCount { get; set; }
+        public HashSet<Coordinate> RemovedThisRound { get; set; } = [];
     }
 }
