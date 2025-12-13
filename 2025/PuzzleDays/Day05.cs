37967623d3a157ccc25253f9445cd72a638fa9cf
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-12-12 22:42:34 -0500

    2025 Day 5 Part 2
    I should really just have a helper for combining ranges by now. Once I got that working it was easy. Didn't help that the example had so few cases to work with

--- 2025/PuzzleDays/Day05.cs
+++ 2025/PuzzleDays/Day05.cs
@@ -29,7 +29,49 @@ public class Day05(ProgressLogger progress) : SingleExecutionPuzzle<Day05.State>
 
     protected override async Task<string> ExecutePuzzlePartTwo()
     {
-        throw new NotImplementedException();
+        var combinedRanges = CondenseRanges(InitialState.FreshRanges);
+
+        // End - start is 1 short for each range to add how many ranges we have
+        var total = combinedRanges.Select(x => x.end - x.start)
+            .Sum() + combinedRanges.Count;
+
+        return total.ToString();
+    }
+
+    private static List<(long start, long end)> CondenseRanges(
+        IEnumerable<(long start, long end)> ranges)
+    {
+        // Order ranges by start
+        var sorted = ranges
+            .OrderBy(r => r.start)
+            .ToList();
+
+        var result = new List<(long start, long end)>
+        {
+            sorted[0]
+        };
+
+        // For the rest
+        foreach (var current in sorted.Skip(1))
+        {
+            // Get the current end of our results
+            // Since they're in order this is the one we could possibly extend
+            var last = result[^1];
+
+            // Overlap or adjacent
+            if (current.start <= last.end)
+            {
+                // Overwrite (can't assign because tuples are value objects)
+                result[^1] = (last.start, Math.Max(last.end, current.end));
+            }
+            // New section in the overall ranges
+            else
+            {
+                result.Add(current);
+            }
+        }
+
+        return result;
     }
 
     protected override async Task<State> LoadInputState(string puzzleInput, PuzzleInputType inputType)
