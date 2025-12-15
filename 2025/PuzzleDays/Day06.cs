e2a1982312e599cb51687c1b27966347526d0cdc
Dylan O'Gara <75340349+HallOfSame@users.noreply.github.com>
2025-12-14 22:14:20 -0500

    2025 Day 6 Part 1
    Easy once I fixed the overflow issues. Wish I could just wrap calling the execute method in checked so I didn't have to always think of that. Part 2 might be tough if this part is so easy when we're technically halfway through now

---
+++ 2025/PuzzleDays/Day06.cs
@@ -0,0 +1,96 @@
+﻿using System.Globalization;
+using Helpers.Structure;
+
+namespace PuzzleDays;
+
+public class Day06 : SingleExecutionPuzzle<Day06.State>
+{
+    public enum Operator
+    {
+        Add,
+        Multiply
+    }
+
+    public override PuzzleInfo Info => new(2025, 6, "Trash Compactor");
+
+    protected override async Task<string> ExecutePuzzlePartOne()
+    {
+        checked
+        {
+            var total = 0m;
+
+            foreach (var problem in InitialState.Problems)
+            {
+                if (problem.Operator == Operator.Add)
+                {
+                    total += problem.Operands.Sum();
+                }
+                else
+                {
+                    total += problem.Operands.Aggregate(1m, (curr, next) => curr * next);
+                }
+            }
+
+            return total.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    protected override async Task<string> ExecutePuzzlePartTwo()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override async Task<State> LoadInputState(string puzzleInput, PuzzleInputType inputType)
+    {
+        var lines = puzzleInput.Split('\n');
+
+        var firstLineSplit = lines[0]
+            .Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
+
+        var problems = new Problem[firstLineSplit.Length];
+        var problemCount = problems.Length;
+
+        for (var i = 0; i < problemCount; i++)
+            problems[i] = new Problem
+            {
+                Operands = [int.Parse(firstLineSplit[i])]
+            };
+
+        foreach (var line in lines.Skip(1)
+                     .Take(lines.Length - 2))
+        {
+            var lineSplit = line.Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
+
+            for (var i = 0; i < problemCount; i++)
+                problems[i]
+                    .Operands.Add(int.Parse(lineSplit[i]));
+        }
+
+        var lastLineSplit = lines[^1]
+            .Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
+
+        for (var i = 0; i < problemCount; i++)
+            problems[i].Operator = lastLineSplit[i] switch
+            {
+                "+" => Operator.Add,
+                "*" => Operator.Multiply,
+                _ => throw new InvalidOperationException($"Invalid operator {lastLineSplit[i]}")
+            };
+
+        return new State
+        {
+            Problems = problems.ToList()
+        };
+    }
+
+    public class State
+    {
+        public required List<Problem> Problems { get; set; }
+    }
+
+    public class Problem
+    {
+        public required List<int> Operands { get; set; }
+        public Operator Operator { get; set; }
+    }
+}
